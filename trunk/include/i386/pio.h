ab151b2b107a746b90ee6ea18b8d334eb2f9f21b
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2009-03-22 09:46:44 +0000

    new headers

--- trunk/include/i386/pio.h
+++ trunk/include/i386/pio.h
@@ -35,12 +35,22 @@
 
 #define SLOW_DOWN_IO __SLOW_DOWN_IO
 
+// see pio.c
+#ifdef __PIO_IMPLEMENTATION
+#define __PIOEXTERN
+#else
+#define __PIOEXTERN static __inline__
+#endif
+
+
+//#define __PIOEXTERN extern inline
+
 /*
  * Talk about misusing macros..
  */
 
 #define __OUT1(s,x) \
-extern inline void __out##s(unsigned x value, unsigned short port) {
+__PIOEXTERN  void __out##s(unsigned x value, unsigned short port) {
 
 #define __OUT2(s,s1,s2) \
 __asm__ __volatile__ ("out" #s " %" s1 "0,%" s2 "1"
@@ -52,7 +62,7 @@ __OUT1(s##_p,x) __OUT2(s,s1,"w") : : "a" (value), "d" (port)); SLOW_DOWN_IO; } \
 __OUT1(s##c_p,x) __OUT2(s,s1,"") : : "a" (value), "id" (port)); SLOW_DOWN_IO; }
 
 #define __IN1(s) \
-extern inline RETURN_TYPE __in##s(unsigned short port) { RETURN_TYPE _v;
+__PIOEXTERN  RETURN_TYPE __in##s(unsigned short port) { RETURN_TYPE _v;
 
 #define __IN2(s,s1,s2) \
 __asm__ __volatile__ ("in" #s " %" s2 "1,%" s1 "0"
@@ -64,12 +74,12 @@ __IN1(s##_p) __IN2(s,s1,"w") : "=a" (_v) : "d" (port) ,##i ); SLOW_DOWN_IO; retu
 __IN1(s##c_p) __IN2(s,s1,"") : "=a" (_v) : "id" (port) ,##i ); SLOW_DOWN_IO; return _v; }
 
 #define __INS(s) \
-extern inline void ins##s(unsigned short port, void * addr, unsigned long count) \
+__PIOEXTERN  void ins##s(unsigned short port, void * addr, unsigned long count) \
 { __asm__ __volatile__ ("cld ; rep ; ins" #s \
 : "=D" (addr), "=c" (count) : "d" (port),"0" (addr),"1" (count)); }
 
 #define __OUTS(s) \
-extern inline void outs##s(unsigned short port, const void * addr, unsigned long count) \
+__PIOEXTERN  void outs##s(unsigned short port, const void * addr, unsigned long count) \
 { __asm__ __volatile__ ("cld ; rep ; outs" #s \
 : "=S" (addr), "=c" (count) : "d" (port),"0" (addr),"1" (count)); }
 
@@ -77,10 +87,12 @@ extern inline void outs##s(unsigned short port, const void * addr, unsigned long
 /* __IN(b,"b","0" (0)) */
 __IN(b,"")
 #undef RETURN_TYPE
+
 #define RETURN_TYPE unsigned short
 /* __IN(w,"w","0" (0)) */
 __IN(w,"")
 #undef RETURN_TYPE
+
 #define RETURN_TYPE unsigned int
 __IN(l,"")
 #undef RETURN_TYPE
@@ -97,12 +109,14 @@ __OUTS(b)
 __OUTS(w)
 __OUTS(l)
 
+
+#if 1
 /*
  * Note that due to the way __builtin_constant_p() works, you
  *  - can't use it inside a inline function (it will never be true)
  *  - you don't have to worry about side effects within the __builtin..
  */
-#define outb(val,port) \
+#define outb(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outbc((val),(port)) : \
 	__outb((val),(port)))
@@ -112,7 +126,7 @@ __OUTS(l)
 	__inbc(port) : \
 	__inb(port))
 
-#define outb_p(val,port) \
+#define outb_p(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outbc_p((val),(port)) : \
 	__outb_p((val),(port)))
@@ -122,7 +136,7 @@ __OUTS(l)
 	__inbc_p(port) : \
 	__inb_p(port))
 
-#define outw(val,port) \
+#define outw(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outwc((val),(port)) : \
 	__outw((val),(port)))
@@ -132,7 +146,7 @@ __OUTS(l)
 	__inwc(port) : \
 	__inw(port))
 
-#define outw_p(val,port) \
+#define outw_p(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outwc_p((val),(port)) : \
 	__outw_p((val),(port)))
@@ -142,7 +156,7 @@ __OUTS(l)
 	__inwc_p(port) : \
 	__inw_p(port))
 
-#define outl(val,port) \
+#define outl(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outlc((val),(port)) : \
 	__outl((val),(port)))
@@ -152,7 +166,7 @@ __OUTS(l)
 	__inlc(port) : \
 	__inl(port))
 
-#define outl_p(val,port) \
+#define outl_p(port,val) \
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__outlc_p((val),(port)) : \
 	__outl_p((val),(port)))
@@ -161,5 +175,8 @@ __OUTS(l)
 ((__builtin_constant_p((port)) && (port) < 256) ? \
 	__inlc_p(port) : \
 	__inl_p(port))
+
+#endif
+
 
 #endif
---
+++ trunk/include/virtio_net.h
@@ -0,0 +1,57 @@
+#ifndef _VIRTIO_NET_H
+#define _VIRTIO_NET_H
+/* This header is BSD licensed so anyone can use the definitions to implement
+ * compatible drivers/servers. */
+#include <phantom_types.h>
+#include <virtio_config.h>
+
+/* The ID for virtio_net */
+#define VIRTIO_ID_NET	1
+
+/* The feature bitmap for virtio net */
+#define VIRTIO_NET_F_CSUM	0	/* Host handles pkts w/ partial csum */
+#define VIRTIO_NET_F_GUEST_CSUM	1	/* Guest handles pkts w/ partial csum */
+#define VIRTIO_NET_F_MAC	5	/* Host has given MAC address. */
+#define VIRTIO_NET_F_GSO	6	/* Host handles pkts w/ any GSO type */
+#define VIRTIO_NET_F_GUEST_TSO4	7	/* Guest can handle TSOv4 in. */
+#define VIRTIO_NET_F_GUEST_TSO6	8	/* Guest can handle TSOv6 in. */
+#define VIRTIO_NET_F_GUEST_ECN	9	/* Guest can handle TSO[6] w/ ECN in. */
+#define VIRTIO_NET_F_GUEST_UFO	10	/* Guest can handle UFO in. */
+#define VIRTIO_NET_F_HOST_TSO4	11	/* Host can handle TSOv4 in. */
+#define VIRTIO_NET_F_HOST_TSO6	12	/* Host can handle TSOv6 in. */
+#define VIRTIO_NET_F_HOST_ECN	13	/* Host can handle TSO[6] w/ ECN in. */
+#define VIRTIO_NET_F_HOST_UFO	14	/* Host can handle UFO in. */
+#define VIRTIO_NET_F_MRG_RXBUF	15	/* Host can merge receive buffers. */
+
+struct virtio_net_config
+{
+	/* The config defining mac address (if VIRTIO_NET_F_MAC) */
+	__u8 mac[6];
+} __attribute__((packed));
+
+/* This is the first element of the scatter-gather list.  If you don't
+ * specify GSO or CSUM features, you can simply ignore the header. */
+struct virtio_net_hdr
+{
+#define VIRTIO_NET_HDR_F_NEEDS_CSUM	1	// Use csum_start, csum_offset
+	__u8 flags;
+#define VIRTIO_NET_HDR_GSO_NONE		0	// Not a GSO frame
+#define VIRTIO_NET_HDR_GSO_TCPV4	1	// GSO frame, IPv4 TCP (TSO)
+#define VIRTIO_NET_HDR_GSO_UDP		3	// GSO frame, IPv4 UDP (UFO)
+#define VIRTIO_NET_HDR_GSO_TCPV6	4	// GSO frame, IPv6 TCP
+#define VIRTIO_NET_HDR_GSO_ECN		0x80	// TCP has ECN set
+	__u8 gso_type;
+	__u16 hdr_len;		/* Ethernet + IP + tcp/udp hdrs */
+	__u16 gso_size;		/* Bytes to append to hdr_len per frame */
+	__u16 csum_start;	/* Position to start checksumming from */
+	__u16 csum_offset;	/* Offset after that to place checksum */
+};
+
+/* This is the version of the header to use when the MRG_RXBUF
+ * feature has been negotiated. */
+struct virtio_net_hdr_mrg_rxbuf {
+	struct virtio_net_hdr hdr;
+	__u16 num_buffers;	/* Number of merged rx buffers */
+};
+
+#endif /* _VIRTIO_NET_H */
