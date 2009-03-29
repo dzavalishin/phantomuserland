cfb77c4d22c03256d7d1b9add735f4c2f0abdfed
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2009-03-29 17:47:17 +0000

    new kernel includes from mach/freebsd

---
+++ include/i386/trap.h
@@ -0,0 +1,78 @@
+/* 
+ * Mach Operating System
+ * Copyright (c) 1991,1990 Carnegie Mellon University
+ * All Rights Reserved.
+ * 
+ * Permission to use, copy, modify and distribute this software and its
+ * documentation is hereby granted, provided that both the copyright
+ * notice and this permission notice appear in all copies of the
+ * software, derivative works or modified versions, and any portions
+ * thereof, and that both notices appear in supporting documentation.
+ * 
+ * CARNEGIE MELLON ALLOWS FREE USE OF THIS SOFTWARE IN ITS "AS IS"
+ * CONDITION.  CARNEGIE MELLON DISCLAIMS ANY LIABILITY OF ANY KIND FOR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Carnegie Mellon requests users of this software to return to
+ * 
+ *  Software Distribution Coordinator  or  Software.Distribution@CS.CMU.EDU
+ *  School of Computer Science
+ *  Carnegie Mellon University
+ *  Pittsburgh PA 15213-3890
+ * 
+ * any improvements or extensions that they make and grant Carnegie Mellon
+ * the rights to redistribute these changes.
+ */
+/*
+ * HISTORY
+ * $Log:	trap.h,v $
+ * Revision 2.5  91/05/14  16:18:26  mrt
+ * 	Correcting copyright
+ * 
+ * Revision 2.4  91/02/05  17:15:33  mrt
+ * 	Changed to new Mach copyright
+ * 	[91/02/01  17:38:51  mrt]
+ * 
+ * Revision 2.3  90/10/25  14:45:03  rwd
+ * 	Added watchpoint support.
+ * 	[90/10/18            rpd]
+ * 
+ * Revision 2.2  90/05/03  15:38:10  dbg
+ * 	Created.
+ * 	[90/02/08            dbg]
+ * 
+ */
+
+#ifndef	_I386_TRAP_H_
+#define	_I386_TRAP_H_
+
+/*
+ * Hardware trap vectors for i386.
+ */
+#define	T_DIVIDE_ERROR		0
+#define	T_DEBUG			1
+#define	T_NMI			2		/* non-maskable interrupt */
+#define	T_INT3			3		/* int 3 instruction */
+#define	T_OVERFLOW		4		/* overflow test */
+#define	T_OUT_OF_BOUNDS		5		/* bounds check */
+#define	T_INVALID_OPCODE	6		/* invalid op code */
+#define	T_NO_FPU		7		/* no floating point */
+#define	T_DOUBLE_FAULT		8		/* double fault */
+#define	T_FPU_FAULT		9
+/*				10 */
+#define	T_SEGMENT_NOT_PRESENT	11
+#define	T_STACK_FAULT		12
+#define	T_GENERAL_PROTECTION	13
+#define	T_PAGE_FAULT		14
+/*				15 */
+#define	T_FLOATING_POINT_ERROR	16
+#define	T_WATCHPOINT		17
+
+/*
+ * Page-fault trap codes.
+ */
+#define	T_PF_PROT		0x1		/* protection violation */
+#define	T_PF_WRITE		0x2		/* write access */
+#define	T_PF_USER		0x4		/* from user state */
+
+#endif	_I386_TRAP_H_
