32a41055b006fb98d0c74e952993cfdf075f26b4
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2009-10-24 20:37:33 +0000

    added headers

---
+++ include/malloc.h
@@ -0,0 +1,12 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <phantom_types.h>
+
+void *malloc(size_t size);
+void free(void *);
+
+void *calloc(size_t n_elem, size_t elem_size);
+
+
+#endif // MALLOC_H
--- include/phantom_libc.h
+++ include/phantom_libc.h
@@ -17,6 +17,7 @@
 
 #include <phantom_types.h>
 #include <stdarg.h>
+#include <malloc.h>
 
 #ifndef NULL
 #define NULL 0
