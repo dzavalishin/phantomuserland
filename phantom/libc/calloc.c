a5a393060f2247d641b45927bd6b893a945d530c
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2009-11-15 12:28:37 +0000

    calloc

---
+++ phantom/libc/calloc.c
@@ -0,0 +1,14 @@
+#include <malloc.h>
+#include <string.h>
+
+void *
+calloc(size_t number, size_t size)
+{
+    void *ret;
+
+    ret = malloc(number * size);
+    if(ret != 0)
+        memset(ret, 0, number * size);
+
+    return ret;
+}
