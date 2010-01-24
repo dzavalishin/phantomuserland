f41de6647401dd9982f0e0392cc0de4a6423c38d
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-01-24 21:42:41 +0000

    page aligned fix, misc func retcodes changed to errno_t

--- trunk/include/kernel/init.h
+++ trunk/include/kernel/init.h
@@ -5,3 +5,6 @@ void phantom_timed_call_init(void);
 void hal_cpu_reset_real(void) __attribute__((noreturn));
 
 void phantom_threads_init();
+
+
+void phantom_load_gdt(void);
--- trunk/include/x86/phantom_page.h
+++ trunk/include/x86/phantom_page.h
@@ -3,4 +3,4 @@
 #define PAGE_SIZE 	4096
 
 #define PAGE_ALIGN(x) (((x) + (PAGE_SIZE-1)) & ~(PAGE_SIZE-1))
-#define PAGE_ALIGNED(x) ( (x)  & (PAGE_SIZE-1) )
+#define PAGE_ALIGNED(x) ( 0 == ( (x)  & (PAGE_SIZE-1) ))
