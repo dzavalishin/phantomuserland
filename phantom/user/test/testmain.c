2ef2fd1de4a8556f449145fd38888fb5b6ff54fd
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-02-13 20:50:28 +0000

    usermode env crt0 fix for __main

--- phantom/user/test/testmain.c
+++ phantom/user/test/testmain.c
@@ -1,8 +1,16 @@
 extern void exit(int);
+extern void sleepmsec(int);
+extern void vsyslog(int level, char *string );
 
 int
-main()
+main(int ac, char **av, char **env)
 {
+	while(1)
+	{
+		vsyslog( 0, "module test is running" );
+		sleepmsec(4000);
+	}
+
 	exit(0);
     asm("int $3");
 }
