dbe55a0c1e4d5b64ee9228533cf33731692beb2c
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-02-14 12:25:10 +0000

    sycalls funcs

--- trunk/phantom/user/test/testmain.c
+++ trunk/phantom/user/test/testmain.c
@@ -1,16 +1,16 @@
+#include <sys/unistd.h>
+
 extern void exit(int);
-extern void sleepmsec(int);
-extern void vsyslog(int level, char *string );
 
 int
 main(int ac, char **av, char **env)
 {
-	while(1)
-	{
-		vsyslog( 0, "module test is running" );
-		sleepmsec(4000);
-	}
+    while(1)
+    {
+        ssyslog( 0, "module test is running" );
+        sleepmsec(4000);
+    }
 
-	exit(0);
+    exit(0);
     asm("int $3");
 }
