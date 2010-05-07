71ce9c48bdd943e0c0bc8becc4021fc1b2277f34
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-05-07 11:12:41 +0000

    Class version .pc field added

--- trunk/plc/src/ru/dz/phantom/code/ClassFileInfo.java
+++ trunk/plc/src/ru/dz/phantom/code/ClassFileInfo.java
@@ -10,29 +10,33 @@ import java.io.*;
  */
 
 public class ClassFileInfo extends ru.dz.phantom.code.FileInfo {
-  String class_name;
-  String class_parent_name;
-  int n_object_slots, n_method_slots;
+	String class_name;
+	String class_parent_name;
+	int n_object_slots, n_method_slots;
+	private final String classVersion;
 
-  public ClassFileInfo(
-      RandomAccessFile os,
-      String class_name,
-      String class_parent_name,
-      int n_object_slots, int n_method_slots
-      )
-  {
-    super( os, (byte)'C' );
-    this.class_name = class_name;
-    this.class_parent_name = class_parent_name;
-    this.n_object_slots = n_object_slots;
-    this.n_method_slots = n_method_slots;
-  }
+	public ClassFileInfo(
+			RandomAccessFile os,
+			String class_name,
+			String class_parent_name,
+			int n_object_slots, int n_method_slots,
+			String classVersion
+	)
+	{
+		super( os, (byte)'C' );
+		this.class_name = class_name;
+		this.class_parent_name = class_parent_name;
+		this.n_object_slots = n_object_slots;
+		this.n_method_slots = n_method_slots;
+		this.classVersion = classVersion;
+	}
 
-  protected void do_write_specific() throws IOException {
-    Fileops.put_string_bin( os, class_name );
-    Fileops.put_int32(os, n_object_slots );
-    Fileops.put_int32(os, n_method_slots );
-    Fileops.put_string_bin( os, class_parent_name );
-  }
+	protected void do_write_specific() throws IOException {
+		Fileops.put_string_bin( os, class_name );
+		Fileops.put_int32(os, n_object_slots );
+		Fileops.put_int32(os, n_method_slots );
+		Fileops.put_string_bin( os, class_parent_name );
+		Fileops.put_string_bin( os, classVersion );
+	}
 
 }
