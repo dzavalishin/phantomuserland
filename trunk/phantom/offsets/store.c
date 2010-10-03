c6c132cee186e75b891f613f513f0e487d010694
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-10-03 09:27:31 +0000

    offset generator warnings cleanup

---
+++ trunk/phantom/offsets/store.c
@@ -0,0 +1,43 @@
+#include "offsets.h"
+
+#include <stdio.h>
+
+//#define FILE void
+
+
+FILE *cout;
+FILE *jout;
+
+
+static void out(const char *type, const char *field, const char *what, int data)
+{
+    static char *pad_spaces = "                                                                                                          ";
+
+    int pad = 60-strlen(type)-strlen(field)-strlen(what);
+    fprintf(cout, "#define %s__%s__%s %.*s%d\n", type, field, what, pad, pad_spaces, data);
+    fprintf(jout, "\tfinal static int %s__%s__%s %.*s= %d;\n", type, field, what, pad, pad_spaces, data);
+}
+
+void out_offset(const char *type, const char *field, int offset)
+{
+    out(type, field, "OFFSET", offset);
+}
+
+
+
+int main(int ac, char **av)
+{
+	cout = fopen("field_offsets.h", "wt");
+	jout = fopen("FieldOffsets.java", "wt");
+
+	fprintf(jout, "package ru.dz.phantom;\n\n");
+	fprintf(jout, "class FieldOffsets {\n\n");
+	generate();
+	fprintf(jout, "\n\n}\n");
+
+	fclose(cout);
+	fclose(jout);
+
+	return (ferror(cout) || ferror(jout)) ? 1 : 0;
+}
+
