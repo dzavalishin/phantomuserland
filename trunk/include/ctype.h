d345932386ae941b73d5721961bec4ad99f20894
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-10-08 11:54:24 +0000

    Some (empty or duplicating) headers

---
+++ trunk/include/ctype.h
@@ -0,0 +1,1 @@
+#include <phantom_libc.h>
---
+++ trunk/include/unistd.h
@@ -0,0 +1,65 @@
+/*
+** Copyright 2001-2004 Travis Geiselbrecht. All rights reserved.
+** Copyright 2002, Manuel J. Petit. All rights reserved.
+** Distributed under the terms of the NewOS License.
+*/
+
+#ifndef __newos__libc_unistd__hh__
+#define __newos__libc_unistd__hh__
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* whence values for lseek() */
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+void    _exit(int);
+
+int     open(char const *, int, ...);
+int     close(int);
+int     dup(int);
+int     dup2(int, int);
+int		unlink(const char *);
+int		fsync(int);
+void	sync(void);
+
+off_t   lseek(int, off_t, int);
+ssize_t read(int, void *, size_t);
+ssize_t pread(int, void *, size_t, off_t);
+ssize_t write(int, void const*, size_t);
+ssize_t pwrite(int, void const*, size_t, off_t);
+
+unsigned sleep(unsigned);
+int      usleep(unsigned);
+
+int   chdir(const char *);
+char *getcwd(char *, size_t);
+char *getwd(char *);
+
+int		pipe(int fds[2]);
+
+/* not strictly supposed to be here, and doesn't quite match unix ioctl() */
+int		ioctl(int, int, void *, size_t);
+
+/* process groups */
+int setpgid(pid_t pid, pid_t pgid);
+pid_t getpgid(pid_t pid);
+int setpgrp(void);
+pid_t getpgrp(void);
+
+/* sessions */
+pid_t getsid(pid_t pid);
+pid_t setsid(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+
+#endif
+
