a64b62fb78753b5d98d4b2edc9e0d5e393935258
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2010-10-08 11:54:24 +0000

    Some (empty or duplicating) headers

--- include/user/sys_fio.h
+++ include/user/sys_fio.h
@@ -1,3 +1,16 @@
+#include <phantom_types.h>
 
-int write( int, void *, int);
-int read( int, void *, int);
+//int write( int, void *, int);
+//int read( int, void *, int);
+
+ssize_t read(int, void *, size_t);
+ssize_t write(int, void const*, size_t);
+
+
+//int open(const char *name, int mode);
+
+int     open(char const *, int, ...);
+
+int close(int fd);
+
+int chdir(const char *name);
