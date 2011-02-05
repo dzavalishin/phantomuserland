10aa3b2940d2391eba24deb27d65d758f743255b
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-02-05 17:35:37 +0000

    Kill trapped thread (instead of panic) if thread is usermode. Untested.

--- trunk/oldtree/kernel/phantom/i386/trap_entry.c
+++ trunk/oldtree/kernel/phantom/i386/trap_entry.c
@@ -1,9 +1,30 @@
 #include <i386/trap.h>
 #include <i386/eflags.h>
 #include <phantom_assert.h>
-//#include <phantom_libc.h>
+#include <phantom_libc.h>
+#include <threads.h>
+#include <thread_private.h>
 
 #include "misc.h"
+
+void
+phantom_check_user_trap( struct trap_state *ts )
+{
+    phantom_thread_t *t = GET_CURRENT_THREAD();
+
+    if(t->thread_flags & THREAD_FLAG_USER)
+    {
+        int tid = t->tid;
+        printf("Usermode thread %d killed due to unexpected trap\n", tid);
+        t_kill_thread( tid );
+        // Will panic below if returned
+        printf("Usermode trap panic in thread %d\n", tid);
+        trap_panic(ts);
+    }
+    // Not user mode, return
+}
+
+
 
 void
 phantom_kernel_trap( struct trap_state *ts )
@@ -16,7 +37,10 @@ phantom_kernel_trap( struct trap_state *ts )
         = phantom_trap_handlers[trapno];
 
     if(handler == 0)
+    {
+        phantom_check_user_trap( ts ); // returns only if not an user mode thread
         trap_panic(ts);
+    }
 
     if(handler(ts))
     {
