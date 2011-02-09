c47903ae732085711aef68eeca534dc97b7133bd
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-02-09 14:06:56 +0000

    Moved exe_module to uu proc hdr

--- include/unix/uuprocess.h
+++ include/unix/uuprocess.h
@@ -1,8 +1,6 @@
 #ifndef UUPROCESS_H
 #define UUPROCESS_H
 
-struct uufile;
-struct uutty;
 
 #define MAX_UU_FD       256
 #define MAX_UU_TID      128
@@ -12,6 +10,51 @@ struct uutty;
 
 // pids before this are reserved
 #define MIN_PID 10
+
+
+
+
+// This structure is pointed by processes that are running it
+// and describes loadable execution unit
+
+// In fact, currently this struct can't be reused because
+// CS and DS are allocated at once
+struct exe_module
+{
+    int                 refcount;
+
+    char                name[MAX_UU_CMD]; // debug only
+
+    u_int32_t           start; // code entry point
+    u_int32_t           esp;
+
+    // possibly here we have to have syscall service pointer
+    //physaddr_t  	phys_cs;
+    u_int16_t           cs_seg;
+    linaddr_t           cs_linear;
+    size_t              cs_pages;
+
+    //physaddr_t  	phys_ds;
+    u_int16_t           ds_seg;
+    linaddr_t           ds_linear;
+    size_t              ds_pages;
+
+	// for pv_free
+	physaddr_t			pa;
+	size_t				mem_size;
+
+    void *              mem_start; // va
+    void *              mem_end; // above last addr
+};
+
+
+
+
+
+
+struct uufile;
+struct uutty;
+
 
 struct uuprocess
 {
@@ -45,11 +88,13 @@ struct uuprocess
 
     void *				mem_start;		// where it starts in linear addr space
     void *				mem_end;		// first address above proc mem
+
+	struct exe_module * em;
 };
 
 typedef struct uuprocess uuprocess_t;
 
-uuprocess_t *uu_create_process(int ppid);
+uuprocess_t *uu_create_process(int ppid, struct exe_module *em);
 void uu_proc_add_thread( uuprocess_t *p, int tid );
 
 
