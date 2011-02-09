4aba00a2248f8dfc068d4cb90c295f6f9b4443c7
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-02-09 14:06:33 +0000

    Cross-page v2p test

--- oldtree/kernel/phantom/test_mem.c
+++ oldtree/kernel/phantom/test_mem.c
@@ -21,6 +21,7 @@
 #include <errno.h>
 #include "test.h"
 
+#include <hal.h>
 
 
 static void *memnotchar(void *addr, int c, size_t size)
@@ -72,4 +73,83 @@ int do_test_malloc(const char *test_parm)
     return 0;
 }
 
+#define MSIZE (4096*2)
+
+int do_test_physmem(const char *test_parm)
+{
+    (void) test_parm;
+
+    void *va;
+    physaddr_t pa;
+
+    char buf[MSIZE];
+
+    hal_pv_alloc( &pa, &va, MSIZE );
+
+    test_check_true( va != 0 );
+    test_check_true( pa != 0 );
+
+    memset( va, 0, MSIZE );
+    memcpy_p2v( buf, pa, MSIZE );
+    if( memnotchar( buf, 0, MSIZE ) )
+        test_fail_msg( EINVAL, "not 0");
+
+
+    memset( buf, 0xFF, MSIZE );
+    memcpy_v2p( pa, buf, MSIZE );
+    if( memnotchar( va, 0xFF, MSIZE ) )
+        test_fail_msg( EINVAL, "not 1");
+
+    memset( va, 0, MSIZE );
+
+    memcpy_v2p( pa, "AAA", 3 );
+    if( memnotchar( va, 'A', 3 ) )
+        test_fail_msg( EINVAL, "not A");
+
+    if( memnotchar( va+3, 0, MSIZE-3 ) )
+        test_fail_msg( EINVAL, "not A0");
+
+
+    memset( va, 0, MSIZE );
+
+    memcpy_v2p( pa+10, "BBB", 3 );
+    if( memnotchar( va+10, 'B', 3 ) )
+        test_fail_msg( EINVAL, "not B");
+
+    if( memnotchar( va, 0, 10 ) )
+        test_fail_msg( EINVAL, "not B0-");
+
+    if( memnotchar( va+13, 0, MSIZE-13 ) )
+        test_fail_msg( EINVAL, "not B0+");
+
+
+    // Cross page
+    memset( va, 0, MSIZE );
+#define SH (4096-4)
+
+    memcpy_v2p( pa+SH, "EEEEEEEE", 8 );
+    if( memnotchar( va+SH, 'E', 8 ) )
+        test_fail_msg( EINVAL, "not E");
+
+    if( memnotchar( va, 0, SH ) )
+        test_fail_msg( EINVAL, "not E0-");
+
+    if( memnotchar( va+SH+8, 0, MSIZE-SH-8 ) )
+        test_fail_msg( EINVAL, "not E0+");
+
+
+
+#if 0 // not impl
+    memset( va, 0, MSIZE );
+
+    memset( va+20, 'C', 3 );
+    memcpy_p2v( buf, pa+20, 3 );
+    if( memnotchar( buf, 'C', 3 ) )
+        test_fail_msg( EINVAL, "not C");
+#endif
+
+    hal_pv_free( pa, va, MSIZE );
+
+    return 0;
+}
 
