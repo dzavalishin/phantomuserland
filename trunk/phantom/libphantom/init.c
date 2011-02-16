f6f30dacb5c7ce7fd7c75459c1f3d053dc01bda0
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-02-16 14:53:10 +0000

    Fixed constructor running code.

--- trunk/phantom/libphantom/init.c
+++ trunk/phantom/libphantom/init.c
@@ -16,12 +16,12 @@
 #include <kernel/init.h>
 
 /* These magic symbols are provided by the linker.  */
-extern void (*__preinit_array_start []) (void) __attribute__((weak));
-extern void (*__preinit_array_end []) (void) __attribute__((weak));
-extern void (*__init_array_start []) (void) __attribute__((weak));
-extern void (*__init_array_end []) (void) __attribute__((weak));
-extern void (*__fini_array_start []) (void) __attribute__((weak));
-extern void (*__fini_array_end []) (void) __attribute__((weak));
+extern void (*_preinit_array_start []) (void) __attribute__((weak));
+extern void (*_preinit_array_end []) (void) __attribute__((weak));
+extern void (*_init_array_start []) (void) __attribute__((weak));
+extern void (*_init_array_end []) (void) __attribute__((weak));
+extern void (*_fini_array_start []) (void) __attribute__((weak));
+extern void (*_fini_array_end []) (void) __attribute__((weak));
 
 //extern void _init (void);
 //extern void _fini (void);
@@ -36,11 +36,22 @@ void __phantom_run_constructors (void)
     for (i = 0; i < count; i++)
         __preinit_array_start[i] ();
 */
-//    _init ();
+    //    _init ();
+
 #if 1
-    count = __init_array_end - __init_array_start;
+    count = _init_array_end - _init_array_start;
+/*
+    printf("%d c'tors (%p - %p) @ (%p - %p)\n", 
+		count,
+		_init_array_start, _init_array_end,
+		&_init_array_start, &_init_array_end
+		);
+*/
     for (i = 0; i < count; i++)
-        __init_array_start[i] ();
+    {
+        //printf("c'tor %p\n", _init_array_start[i]);
+        _init_array_start[i] ();
+    }
 #endif
 }
 
