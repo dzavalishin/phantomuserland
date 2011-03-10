2358b649bc5c162f7525128295985aee9ad046a4
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-03-10 10:15:30 +0000

    Shell script to create phantom disk image, correct superblock in img/

---
+++ trunk/run/zero_ph_img.sh
@@ -0,0 +1,6 @@
+rm phantom.img
+touch phantom.img
+dd bs=4096 seek=0 count=20480 if=/dev/zero of=phantom.img
+dd conv=nocreat conv=notrunc bs=4096 count=1 seek=16 if=img/phantom.superblock of=phantom.img
+# dd if=/dev/zero of=snapcopy.img bs=4096 skip=1 count=1024
+dd if=/dev/zero of=vio.img bs=4096 skip=1 count=1024
