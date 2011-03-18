595c4a1a93eb66ab91463bf279755b754ddced50
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-03-18 12:22:35 +0000

    arm paging code, prelim, untested

--- oldtree/kernel/phantom/arm/tlb.c
+++ oldtree/kernel/phantom/arm/tlb.c
@@ -8,13 +8,7 @@
  *
 **/
 
-#include <kernel/mmu.h>
+//#include <kernel/mmu.h>
 
-/*
- * Inline function to flush a page from the TLB.
- */
-void ftlbentry(physaddr_t la)
-{
-#warning TLB Flush!
-    //asm volatile("invlpg (%0)" : : "r" (la) : "memory");
-}
+// see arm/paging.c
+
