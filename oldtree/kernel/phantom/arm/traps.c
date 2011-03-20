feb85718ed52b00dd38ef1007a7432cf1ea2bffd
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-03-20 09:47:15 +0000

    arm trap/interrupt entry code, untested.

---
+++ oldtree/kernel/phantom/arm/traps.c
@@ -0,0 +1,11 @@
+
+
+/*
+
+STMFD, STMDB, LDMEA, LDMDB:
+Starts from highest reg number. Highest reg number in highest mem addr.
+
+LDMIA, LDMFD, STMIA, STMEA:
+Startts from lowest reg number. Highest reg number in highest mem addr.
+
+*/
