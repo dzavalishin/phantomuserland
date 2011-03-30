e74a20fb0a74133424b12b43114e72c055ba3762
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-03-30 10:39:43 +0000

    amd64 cleanup

--- trunk/phantom/libc/memset.c
+++ trunk/phantom/libc/memset.c
@@ -105,7 +105,7 @@ memset(void *dst0, int c0, size_t length)
 	}
 #endif
 	/* Align destination by filling in bytes. */
-	if ((t = (long)dst & wmask) != 0) {
+	if ((t = (size_t)dst & wmask) != 0) {
 		t = wsize - t;
 		length -= t;
 		do {
