6fc898c047c96da7ae8e62e8e577eab89d02b351
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-05-17 12:17:15 +0000

    HexView (incomplete)

--- trunk/tools/pdb/src/ru/dz/pdb/phantom/ObjectHeader.java
+++ trunk/tools/pdb/src/ru/dz/pdb/phantom/ObjectHeader.java
@@ -7,19 +7,21 @@ import java.util.Vector;
 
 public class ObjectHeader {
 
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_FINALIZER 		= 0x1000;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CHILDFREE  	= 0x800;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_THREAD  		= 0x400;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_STACK_FRAME 	= 0x200;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CALL_FRAME 	= 0x100;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERNAL 		= 0x80;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_RESIZEABLE 	= 0x40;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_STRING 		= 0x20;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INT  			= 0x10;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_DECOMPOSEABLE 	= 0x08;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CLASS 			= 0x04;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERFACE 		= 0x02;
-	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CODE  			= 0x01;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_IMMUTABLE       = 0x2000;
+
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_FINALIZER       = 0x1000;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CHILDFREE       = 0x800;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_THREAD          = 0x400;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_STACK_FRAME     = 0x200;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CALL_FRAME      = 0x100;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERNAL        = 0x80;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_RESIZEABLE      = 0x40;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_STRING          = 0x20;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INT             = 0x10;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_DECOMPOSEABLE   = 0x08;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CLASS           = 0x04;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERFACE       = 0x02;
+	public static final int  PHANTOM_OBJECT_STORAGE_FLAG_IS_CODE            = 0x01;
 
 	public final int PVM_OBJECT_START_MARKER = 0x7FAA7F55;
 	private static final int REF_BYTES = 8; // TODO - hardcoded size
@@ -176,19 +178,21 @@ public class ObjectHeader {
 	public String getFlagsList() {
 		StringBuilder sb = new StringBuilder();
 
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_FINALIZER))		sb.append("Fin "); 		
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CHILDFREE))  	sb.append("NoChild ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_THREAD))  		sb.append("Thread ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_STACK_FRAME)) 	sb.append("SFrame ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CALL_FRAME)) 	sb.append("CFrame ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERNAL)) 		sb.append("Internal ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_RESIZEABLE)) 	sb.append("Resize ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_STRING)) 		sb.append("String ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INT))  			sb.append("Int ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_DECOMPOSEABLE)) 	sb.append("Decomp ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CLASS)) 			sb.append("Class ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERFACE)) 		sb.append("IFace ");
-		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CODE))  			sb.append("Code ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_IMMUTABLE))       sb.append("Imm?! ");
+
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_FINALIZER))       sb.append("Fin "); 		
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CHILDFREE))       sb.append("NoChild ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_THREAD))          sb.append("Thread ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_STACK_FRAME))     sb.append("SFrame ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CALL_FRAME))      sb.append("CFrame ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERNAL))        sb.append("Internal ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_RESIZEABLE))      sb.append("Resize ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_STRING))          sb.append("String ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INT))             sb.append("Int ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_DECOMPOSEABLE))   sb.append("Decomp ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CLASS))           sb.append("Class ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_INTERFACE))       sb.append("IFace ");
+		if(0 != (objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CODE))            sb.append("Code ");
 
 		return sb.toString();
 	}
@@ -213,7 +217,8 @@ public class ObjectHeader {
 			else if((objectFlags & PHANTOM_OBJECT_STORAGE_FLAG_IS_CODE) != 0)
 			{
 				// Code
-				throw new InvalidObjectOperationException("No avatar for unknown internal");			
+				return new CodeObject(this); 
+				//throw new InvalidObjectOperationException("No avatar for unknown internal");			
 			}
 			else
 			{
--- trunk/tools/pdb/src/ru/dz/pdb/ui/bits/HexView.java
+++ trunk/tools/pdb/src/ru/dz/pdb/ui/bits/HexView.java
@@ -1,23 +1,76 @@
 package ru.dz.pdb.ui.bits;
 
-public class HexView {
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class HexView extends JComponent 
+{
+	private static final int N_CHARS_PER_LINE = 16;
+	private final byte[] data;
+
+	public HexView(byte[] data) {
+		this.data = data;
+		setSize(getLineWidth(),getNLines()*getLineHeight());
+		//setVisible(true);
+		repaint();
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		super.paintComponent(g);
+		
+		g.setColor(Color.lightGray);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		int lineHeight = getLineHeight();
+		int lineWidth = getLineWidth();
+		
+		g.setColor(Color.black);
+		int n_lines = getNLines();
+		
+		for(int i = 0; i < n_lines; i++)
+		{
+			drawLine(i,g2d,lineHeight);
+		}
+	}
 
 
 
 
+	private int getNLines() {
+		return 1+((data.length-1)/N_CHARS_PER_LINE);
+	}
+
+	private void drawLine(int i, Graphics2D g2d, int height) {
+		String line = byteArrayToHexString(data, i*N_CHARS_PER_LINE, N_CHARS_PER_LINE);
+		System.out.println("HexView.drawLine(\""+line+"\")");
+		g2d.drawString(line, 4, i*height);
+	}
+
+	private int getLineWidth() {
+		return N_CHARS_PER_LINE*32;
+	}
+
+
+	private int getLineHeight() {
+		return 20;		
+	}
 
 
 
-
-
-
-	
+	/*
 	static final String HEXCHARS[] = {
 		"0", "1", "2", "3", "4", "5", "6", "7", 
 		"8", "9", "A", "B", "C", "D", "E", "F"
-		};
+	};
+	*/
 
-	
 	/**
 	 * Convert a byte[] array to hex dump.
 	 * 
@@ -25,7 +78,7 @@ public class HexView {
 	 * @param in byte[] buffer to convert to string format
 	 */
 
-	static String byteArrayToHexString( byte in[] ) 
+	static String byteArrayToHexString( byte in[], int start, int len ) 
 	{
 		byte ch;
 		int i; 
@@ -33,29 +86,36 @@ public class HexView {
 		if (in == null || in.length <= 0)
 			return null;
 
-		StringBuffer out = new StringBuffer(in.length * 3 + 2);
+		if(start+len > in.length)
+			len = in.length-start;
+		
+		StringBuilder out = new StringBuilder(in.length * 4 + 2);
 
-
-
-		for(i = 0; i < in.length; i++) 
+		for(i = start; i < start+len; i++) 
 		{
-
-			ch = (byte) ((in[i] >> 4) & 0x0F);
-			out.append(HEXCHARS[ (int) ch]); // convert the			nibble to a String Character
-
-			ch = (byte) (in[i] & 0x0F); // Strip off			low nibble 
-			out.append(HEXCHARS[ (int) ch]); // convert the			nibble to a String Character
+			out.append(String.format("%02X", in[i]));
 		}
 
 		out.append(' ');
-		
-		for(i = 0; i < in.length; i++) 
+
+		for(i = start; i < start+len; i++) 
 		{
-			out.append((char)in[i]);
+			char c = (char)in[i];
+			if( in[i] < 32) c = '.';
+			out.append(c);
 		}
-		
+
 		return out.toString();
 	}    
 
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		
+		byte[] t = {0,1,2,0x4E};
+		f.add(new HexView(t));
+		f.pack();
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
