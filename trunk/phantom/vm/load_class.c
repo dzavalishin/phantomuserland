a015a1b5609db3bf7941a22484c74d2955050c12
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-05-26 11:30:55 +0000

    load method sig, field name/type

--- trunk/phantom/vm/load_class.c
+++ trunk/phantom/vm/load_class.c
@@ -48,6 +48,43 @@ struct method_loader_handler
     int            			my_ordinal;
 };
 
+
+struct type_loader_handler
+{
+    struct pvm_code_handler 		ch;
+
+    struct pvm_object 			class_name;
+    struct pvm_object 			contained_class_name;
+    int            			is_container;
+};
+
+
+static void
+pvm_load_type( struct pvm_code_handler *h , struct type_loader_handler *th )
+{
+    th->is_container = pvm_code_get_int32(h);
+    th->class_name = pvm_code_get_string(h);
+    th->contained_class_name = pvm_code_get_string(h);
+}
+
+static void
+pvm_dump_type( struct type_loader_handler *th )
+{
+    //printf("%s ", th->is_container ? "cont" : "" );
+
+    pvm_object_print( th->class_name );
+
+    if(th->is_container)
+    {
+        printf("[ ");
+        pvm_object_print( th->contained_class_name );
+        printf(" ]");
+    }
+
+}
+
+
+
 static void
 pvm_load_method( struct method_loader_handler *mh, const unsigned char *data, int in_size )
 {
@@ -91,6 +128,7 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
     struct pvm_object iface;
     struct pvm_object ip2line_maps;
     struct pvm_object method_names;
+    struct pvm_object field_names;
 
     int got_class_header = 0;
 
@@ -121,6 +159,8 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
             return 1;
         }
 
+        const int record_data_size = record_size - (ptr-rec_start);
+
         switch( record_type )
         {
         case 'C': // class
@@ -128,7 +168,7 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
                 struct pvm_code_handler h;//( ptr, record_size - (ptr-rec_start));
                 h.IP = 0;
                 h.code = ptr;
-                h.IP_max = record_size - (ptr-rec_start);
+                h.IP_max = record_data_size;
 
                 if(debug_print) printf("Class is: " );
 
@@ -142,7 +182,7 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
                 if(debug_print) printf(", %d methods\n", n_method_slots );
 
                 struct pvm_object base_name = pvm_code_get_string(&h);
-                //if(debug_print)
+                if(debug_print)
                 {
                     printf("Based on: ");
                     pvm_object_print( base_name );
@@ -168,24 +208,19 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
                 iface = pvm_create_interface_object( n_method_slots, base_class );
                 ip2line_maps = pvm_object_create_dynamic( pvm_get_array_class(), n_method_slots );
                 method_names = pvm_object_create_dynamic( pvm_get_array_class(), n_method_slots );
+                field_names = pvm_object_create_dynamic( pvm_get_array_class(), n_object_slots );
 
             }
             break;
 
-        case 'M': // class
+        case 'M': // method
             {
                 struct method_loader_handler mh;
-                pvm_load_method( &mh, ptr, record_size - (ptr-rec_start));
+                pvm_load_method( &mh, ptr, record_data_size );
                 //methods.push_back(mh);
                 //iface.save( mh.my_ordinal, mh.my_code );
                 pvm_set_ofield( iface, mh.my_ordinal, mh.my_code );
                 pvm_set_ofield( method_names, mh.my_ordinal, mh.my_name );
-            }
-            break;
-
-        case 'S': // method signature
-            {
-                if(debug_print) printf("\n" );
             }
             break;
 
@@ -194,7 +229,7 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
                 struct pvm_code_handler h;
                 h.IP = 0;
                 h.code = ptr;
-                h.IP_max = record_size - (ptr-rec_start);
+                h.IP_max = record_data_size;
 
                 if(debug_print) printf("line num map\n" );
                 int ordinal = pvm_code_get_int32(&h);
@@ -220,6 +255,150 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
                 pvm_set_ofield( ip2line_maps, ordinal, map );
             }
             break;
+
+        case 'S': // method signature
+            {
+                struct pvm_code_handler h;
+                h.IP = 0;
+                h.code = ptr;
+                h.IP_max = record_data_size;
+
+                if(debug_print) printf("meth sig\n" );
+
+                //hexdump(ptr, record_data_size, "", 0 );
+
+                pvm_object_t m_name = pvm_code_get_string(&h);
+                int m_ordinal = pvm_code_get_int32(&h);
+                int m_n_args = pvm_code_get_int32(&h);
+
+
+
+                    /*
+                    int t_is_container = pvm_code_get_int32(&h);
+                    pvm_object_t t_class_name = pvm_code_get_string(&h);
+                    pvm_object_t t_contained_name = pvm_code_get_string(&h);
+
+                    printf("%s ", t_is_container ? " cont" : "" );
+
+                    pvm_object_print( t_class_name );
+
+                    if(t_is_container)
+                    {
+                        printf(" contaied type ");
+                        pvm_object_print( t_contained_name );
+                        }*/
+                    struct type_loader_handler mth;
+                    pvm_load_type( &h , &mth );
+
+
+
+
+                if(debug_print)
+                {
+                    printf("Method ");
+                    pvm_dump_type( &mth );
+                    printf(" '");
+                    pvm_object_print( m_name );
+                    printf("' ord %d args %d ( ", m_ordinal, m_n_args );
+                }
+
+                int i = m_n_args;
+                while(i-- > 0)
+                {
+                    pvm_object_t a_name = pvm_code_get_string(&h);
+
+                    //int t_is_container = pvm_code_get_int32(&h);
+                    //pvm_object_t t_class_name = pvm_code_get_string(&h);
+                    //pvm_object_t t_contained_name = pvm_code_get_string(&h);
+                    struct type_loader_handler th;
+                    pvm_load_type( &h , &th );
+
+
+                    if(debug_print)
+                    {
+                        pvm_object_print( a_name );
+                        printf(" : " );
+                        pvm_dump_type( &th );
+                        if(i > 0 )
+                            printf(", ");
+                    }
+
+                    /*
+                    printf(" %s: ", t_is_container ? " cont" : "" );
+
+                    pvm_object_print( t_class_name );
+
+                    if(t_is_container)
+                    {
+                        printf(" contaied type ");
+                        pvm_object_print( t_contained_name );
+                    }*/
+
+                }
+
+                if(debug_print) printf(" )\n");
+
+            }
+            break;
+
+
+        case 'f': // field names
+            {
+                struct pvm_code_handler h;//( ptr, record_size - (ptr-rec_start));
+                h.IP = 0;
+                h.code = ptr;
+                h.IP_max = record_data_size;
+
+                if(debug_print) printf("fields\n" );
+                //hexdump(ptr, record_data_size, "", 0 );
+
+                while( h.IP < h.IP_max )
+                {
+                    pvm_object_t f_name = pvm_code_get_string(&h);
+                    int f_ordinal = pvm_code_get_int32(&h);
+
+                    /*
+                    int t_is_container = pvm_code_get_int32(&h);
+                    pvm_object_t t_class_name = pvm_code_get_string(&h);
+                    pvm_object_t t_contained_name = pvm_code_get_string(&h);
+                    */
+
+                    struct type_loader_handler th;
+                    pvm_load_type( &h , &th );
+
+                    if(debug_print)
+                    {
+                        printf("Field '");
+                        pvm_object_print( f_name );
+
+                    //printf("' ord %d%s type: ", f_ordinal, t_is_container ? " cont" : "" );
+                        printf("' ord %d type: ", f_ordinal );
+
+                        pvm_dump_type( &th );
+                        printf("\n");
+                    }
+                    /*
+                    pvm_object_print( t_class_name );
+
+                    if(t_is_container)
+                    {
+                        printf(" contaied type ");
+                        pvm_object_print( t_contained_name );
+                        }
+                    */
+
+
+                    pvm_set_ofield( field_names, f_ordinal, f_name );
+
+                }
+
+            }
+            break;
+
+        default:
+            {
+                printf("Class record '%c' ignored\n", record_type );
+            }
         }
 
     }
@@ -244,6 +423,7 @@ int pvm_load_class_from_memory( const void *data, int fsize, struct pvm_object *
     struct data_area_4_class *cda= pvm_object_da( new_class, class );
     cda->ip2line_maps = ip2line_maps;
     cda->method_names = method_names;
+    cda->field_names = field_names;
 
     *out = new_class;
     return 0;
