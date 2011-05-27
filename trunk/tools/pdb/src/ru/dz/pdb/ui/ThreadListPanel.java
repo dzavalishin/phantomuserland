b212890f3552c671bb9dbf3aaac2ac38bfc88809
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-05-27 07:27:33 +0000

    pdb thread info display (need transport yet)

--- trunk/tools/pdb/src/ru/dz/pdb/ui/ThreadListPanel.java
+++ trunk/tools/pdb/src/ru/dz/pdb/ui/ThreadListPanel.java
@@ -1,8 +1,9 @@
 package ru.dz.pdb.ui;
 
-import java.awt.Dimension;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
@@ -10,28 +11,29 @@ import javax.swing.table.AbstractTableModel;
 
 import ru.dz.pdb.CmdException;
 import ru.dz.pdb.Main;
+import ru.dz.pdb.phantom.ObjectRef;
+import ru.dz.pdb.ui.ThreadListTableModel.ThreadInfo;
 
-public class ThreadListPanel extends JPanel {
+public class ThreadListPanel extends JTable {
 
-	private ThreadListTableModel dm = new ThreadListTableModel();
-	private List<Integer> threadList = new LinkedList<Integer>();
+	//private ThreadListTableModel dm = new ThreadListTableModel();
+	//private List<Integer> threadList = new LinkedList<Integer>();
 	//private TableColumnModel cm = new ThreadListTableColumnModel();
-
-	private static final String [] columnNames = {"tid"};
 
 	
 	public ThreadListPanel() {
 		//JTable t = new JTable(dm,cm);
-		JTable t = new JTable(dm);
-		add(t);
-				
+		//super(dm);
+		super(new ThreadListTableModel());
+		//add(t);
+		/*		
 		try {
 			threadList = Main.getThreadList();
 		} catch (CmdException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
 		//Dimension preferredSize = new Dimension(300, 180);
 		//t.setPreferredSize(preferredSize);
 
@@ -41,74 +43,123 @@ public class ThreadListPanel extends JPanel {
 		t.addColumn(c1 );
 		*/
 		
-		t.setFillsViewportHeight(true);
+		setFillsViewportHeight(true);
 
-		t.setShowGrid(true);
-		t.setAutoCreateColumnsFromModel(true);
-		t.setRowSelectionAllowed(true);
+		setShowGrid(true);
+		//t.setAutoCreateColumnsFromModel(true);
+		//t.setRowSelectionAllowed(true);
 		
 	}
 	
 	
-	class ThreadListTableModel extends AbstractTableModel
-	{
-		
-		@Override
-		public int getColumnCount() { return columnNames.length; }
 
-		@Override
-		public String getColumnName(int col) {
-			//return "tid";
-			//System.out.println("ThreadListPanel.ThreadListTableModel.getColumnName()"+col);
-			return columnNames[col];
+}
+
+
+class ThreadListTableModel extends AbstractTableModel
+{
+	private List<Integer> threadList = new LinkedList<Integer>();
+	private Map<Integer,ThreadInfo> threadInfoList = new TreeMap<Integer,ThreadInfo>();
+	
+	public ThreadListTableModel() {
+		try {
+			threadList = Main.getThreadList();
+		} catch (CmdException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	private static final String [] columnNames = {"tid","ref"};
 
-		@Override
-		public int getRowCount() {
-			//System.out.println("ThreadListPanel.ThreadListTableModel.getRowCount() = "+threadList.size());
-			return threadList.size();
-		}
+	
+	@Override
+	public int getColumnCount() { return columnNames.length; }
 
-		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {
+	@Override
+	public String getColumnName(int col) {
+		//return "tid";
+		//System.out.println("ThreadListPanel.ThreadListTableModel.getColumnName()"+col);
+		return columnNames[col];
+	}
+
+	@Override
+	public int getRowCount() {
+		//System.out.println("ThreadListPanel.ThreadListTableModel.getRowCount() = "+threadList.size());
+		return threadList.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if(columnIndex == 0)
 			return threadList.get(rowIndex);
+
+		ThreadInfo info = threadInfoList.get(rowIndex);
+		if(info == null)
+		{
+			//info = getThreadInfo(threadList.get(rowIndex));
+			info = new ThreadInfo(threadList.get(rowIndex));
+			threadInfoList.put(rowIndex,info);
+		}
+		
+		switch(columnIndex)
+		{
+		case 1:
+			return info.getRef();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		// no way
+		return false;
+	}
+
+
+	/*
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch(columnIndex)
+		{
+		case 0:
+			return Integer.class;
+		}
+		return null;
+	}
+	
+	@Override
+	public void addTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		// ignore			
+	}
+	*/
+	
+	
+	class ThreadInfo
+	{
+		private final int tid;
+
+		public ThreadInfo(int tid) {
+			this.tid = tid;
+			
 		}
 
-		@Override
-		public boolean isCellEditable(int rowIndex, int columnIndex) {
-			// no way
-			return false;
-		}
-
-
-		/*
-		@Override
-		public Class<?> getColumnClass(int columnIndex) {
-			switch(columnIndex)
-			{
-			case 0:
-				return Integer.class;
-			}
+		public ObjectRef getRef() {
+			// TODO Auto-generated method stub
 			return null;
 		}
-		
-		@Override
-		public void addTableModelListener(TableModelListener l) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeTableModelListener(TableModelListener l) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-			// ignore			
-		}
-		*/
 	}
-
 }
