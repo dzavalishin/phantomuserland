ad27ca9e242fac08894f7d07adf4a268d562b663
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-07-09 14:50:13 +0000

    mass win repaint thread

---
+++ phantom/vm/wpaint.c
@@ -0,0 +1,135 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2010 Dmitry Zavalishin, dz@dz.ru
+ *
+ * (new) Windowing system internals and housekeeping.
+ *
+ *
+**/
+
+#include <drv_video_screen.h>
+#include <assert.h>
+//#include <kernel/pool.h>
+#include <kernel/libkern.h>
+#include <kernel/sem.h>
+#include <threads.h>
+#include <phantom_libc.h>
+
+static void paint_thread(void *arg);
+static void start_paint_thread(void);
+
+// max 0.2 sec without paint
+#define MAX_NO_PAINT 1900
+
+static int paint_tid = -1;
+
+static int paint_request = 0;
+static int paint_all = 0;
+static hal_sem_t paint_sem;
+
+static int prev_paint_request = 0;
+static int no_paint_msec = 0;
+static rect_t total;
+
+#define CHECK_START() ({ if(paint_tid < 0) start_paint_thread(); })
+
+// TODO need mutex to access rect
+
+void w_request_async_repaint( rect_t *r )
+{
+    CHECK_START();
+
+    rect_t old_total = total;
+    int old_pq = paint_request;
+
+    rect_add( &total, &old_total, r );
+
+    // Other thread changed something, we have collision.
+    // For now just repaint all the screen.
+    if(old_pq != paint_request)
+        paint_all++;
+
+    paint_request++; // TODO need atomic
+    hal_sem_release( &paint_sem );
+}
+
+#if SCREEN_UPDATE_THREAD
+void drv_video_window_repaint_all(void)
+{
+    CHECK_START();
+    paint_all++;
+    paint_request++; // TODO need atomic
+    hal_sem_release( &paint_sem );
+}
+#endif
+
+static void do_paint_area()
+{
+    rect_t todo = total;
+    int old_pq = paint_request;
+
+    total.ysize = total.ysize = 0;
+
+    // Other thread changed something, we have collision.
+    // For now just repaint all the screen.
+    if(old_pq != paint_request)
+        paint_all++;
+
+    if( paint_all )
+    {
+        paint_all = 0;
+        todo.x = todo.y = 0;
+        todo.xsize = get_screen_xsize();
+        todo.ysize = get_screen_ysize();
+    }
+
+    repaint_all_for_square(&todo);
+}
+
+
+
+static void paint_thread(void *arg)
+{
+    (void) arg;
+
+    hal_set_current_thread_name("paint");
+    // +1 so that it is a bit higher than regular sys threads
+    hal_set_current_thread_priority(PHANTOM_SYS_THREAD_PRIO+1);
+
+    while(1)
+    {
+        //hal_sleep_msec(10);
+        hal_sem_acquire( &paint_sem );
+        if( !paint_request )
+            continue;
+
+        if( prev_paint_request < paint_request )
+        {
+            if( no_paint_msec < MAX_NO_PAINT )
+            {
+                prev_paint_request = paint_request;
+                no_paint_msec += 10;
+                continue;
+            }
+        }
+
+        prev_paint_request = paint_request = 0;
+        no_paint_msec = 0;
+
+        do_paint_area();
+
+    }
+
+}
+
+static void start_paint_thread(void)
+{
+    hal_sem_init( &paint_sem, "paint" );
+    paint_tid = hal_start_thread( paint_thread, 0, 0 );
+}
+
+
+
+
