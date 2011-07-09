54f849dc9a550eedddcd2bbc1f39ed4d6785b9f4
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-07-09 15:15:53 +0000

    moved trap2signo to ia32, minor change in spinlock debug

--- oldtree/kernel/phantom/ia32/traps.c
+++ oldtree/kernel/phantom/ia32/traps.c
@@ -2,13 +2,12 @@
  *
  * Phantom OS
  *
- * Copyright (C) 2005-2010 Dmitry Zavalishin, dz@dz.ru
+ * Copyright (C) 2005-2011 Dmitry Zavalishin, dz@dz.ru
  *
- * ia32 traps support. TODO Must go to ARCH dir!
+ * ia32 traps support.
  *
 **/
 
-//#include <i386/trap.h>
 #include <i386/proc_reg.h>
 #include <i386/eflags.h>
 
@@ -16,6 +15,10 @@
 
 #include <phantom_assert.h>
 #include <phantom_libc.h>
+
+#include <signal.h>
+//#include <i386/proc_reg.h>
+#include <mach/debug_reg.h>
 
 static char *trap_type[] = {
     "Divide error", "Debug trap",		"NMI",		"Breakpoint",
@@ -149,3 +152,52 @@ int (*phantom_trap_handlers[ARCH_N_TRAPS])(struct trap_state *ts) = {
 	trap_panic  /* 31 */
 };
 
+
+
+
+int trap2signo( struct trap_state *ts )
+{
+    int		sig_no = 0;
+
+    /*
+     * It probably makes no sense to pass all these signals to the
+     * application program.
+     */
+    switch (ts->trapno)
+    {
+
+    case -1: 	sig_no = SIGINT;  break;	/* hardware interrupt */
+    case 0: 	sig_no = SIGFPE;  break;	/* divide by zero */
+    case 1: 	sig_no = SIGTRAP; break;	/* debug exception */
+    case 3: 	sig_no = SIGTRAP; break;	/* breakpoint */
+    case 4: 	sig_no = SIGFPE;  break;	/* overflow */
+    case 5: 	sig_no = SIGFPE;  break;	/* bound instruction */
+    case 6: 	sig_no = SIGILL;  break;	/* Invalid opcode */
+    case 7: 	sig_no = SIGFPE;  break;	/* coprocessor not available */
+    case 9: 	sig_no = SIGBUS;  break;	/* coproc segment overrun*/
+    case 10: 	sig_no = SIGBUS;  break;	/* Invalid TSS */
+    case 11: 	sig_no = SIGSEGV; break;	/* Segment not present */
+    case 12: 	sig_no = SIGSEGV; break;	/* stack exception */
+    case 13: 	sig_no = SIGSEGV; break;	/* general protection */
+    case 14: 	sig_no = SIGSEGV; break;	/* page fault */
+    case 16: 	sig_no = SIGFPE;  break;	/* coprocessor error */
+
+    default:
+        panic("No signal mapping for trap number: %d", ts->trapno);
+    }
+
+    /*
+     * Look for null pointer exceptions ...
+     */
+    if(ts->trapno == 1)
+    {
+        unsigned dr6 = rdr6();
+
+        if (dr6 & (DR6_B0|DR6_B1))
+            sig_no = SIGSEGV;
+        else
+            sig_no = SIGTRAP;
+    }
+
+    return sig_no;
+}
