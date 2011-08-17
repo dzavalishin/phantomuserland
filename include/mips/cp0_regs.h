acf1ff2fcdf0a20f0a2ffc32811fb4b6a3cbfec4
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-08-17 06:18:37 +0000

    MIPS machdep includes

---
+++ include/mips/cp0_regs.h
@@ -0,0 +1,63 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2011 Dmitry Zavalishin, dz@dz.ru
+ *
+ * MIPS coprocessor 0 regs. 
+ *
+**/
+
+#ifndef MIPS_CP0_REGS
+#define MIPS_CP0_REGS
+
+#define CP0_INDEX       $0
+#define CP0_RANDOM      $1
+#define CP0_ENTRYLO0    $2
+#define CP0_ENTRYLO1    $3
+#define CP0_CONTEXT     $4
+#define CP0_PAGEMASK    $5
+#define CP0_WIRED $6
+#define CP0_BADVADDR $8
+#define CP0_COUNT $9
+#define CP0_ENTRYHI $10
+#define CP0_COMPARE $11
+#define CP0_STATUS $12
+#define CP0_CAUSE $13
+#define CP0_EPC $14
+#define CP0_PRID $15
+#define CP0_CONFIG $16
+#define CP0_LLADDR $17
+#define CP0_WATCHLO $18
+#define CP0_WATCHHI $19
+#define CP0_XCONTEXT $20
+#define CP0_FRAMEMASK $21
+#define CP0_DIAGNOSTIC $22
+#define CP0_PERFORMANCE $25
+#define CP0_ECC $26
+#define CP0_CACHEERR $27
+#define CP0_TAGLO $28
+#define CP0_TAGHI $29
+#define CP0_ERROREPC $30
+
+/*
+ * Status register.
+ */
+#define ST_IE		0x00000001	/* разрешение прерываний */
+#define ST_EXL		0x00000002	/* уровень исключения */
+#define ST_ERL		0x00000004	/* уровень ошибки */
+#define ST_UM		0x00000010	/* режим пользователя */
+#define ST_IM_SW0	0x00000100	/* программное прерывание 0 */
+#define ST_IM_SW1	0x00000200	/* программное прерывание 1 */
+
+#define ST_NMI		0x00080000	/* причина сброса - NMI */
+#define ST_TS		0x00200000	/* TLB-закрытие системы */
+#define ST_BEV		0x00400000	/* размещение векторов: начальная загрузка */
+
+#define ST_CU0		0x10000000	/* разрешение сопроцессора 0 */
+#define ST_CU1		0x20000000	/* разрешение сопроцессора 1 (FPU) */
+
+
+
+#endif // MIPS_CP0_REGS
+
