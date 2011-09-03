253f8eed354621afe4efdffbf3645a62e0f16811
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-09-03 21:32:21 +0000

    ia32 per cpu TSS extern

--- trunk/include/ia32/private.h
+++ trunk/include/ia32/private.h
@@ -3,9 +3,13 @@
 #endif
 
 #include <ia32/seg.h>
+#include <ia32/tss.h>
 
 void phantom_load_main_tss(void);
 void set_descriptor_limit( struct real_descriptor *d, unsigned limit );
 
 extern struct real_descriptor 	gdt[];
 //struct real_descriptor 	ldt[LDTSZ];
+
+extern struct i386_tss	       	cpu_tss[];
+//extern struct vm86tss		tss_vm86;
