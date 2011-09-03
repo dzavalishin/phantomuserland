b96956148663ad38adbecdd8648c0dc6dcc9c42c
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-09-03 21:42:01 +0000

    vm mutex/cond/sema, atomic sleep/spin unlock - kernel

--- trunk/oldtree/kernel/phantom/snap_sync.c
+++ trunk/oldtree/kernel/phantom/snap_sync.c
@@ -1,11 +1,28 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2011 Dmitry Zavalishin, dz@dz.ru
+ *
+ * VM threads sleep/wakeup/snap synchronization
+ *
+ *
+**/
+
+#define DEBUG_MSG_PREFIX "vm.sync"
+#include <debug_ext.h>
+#define debug_level_flow 6
+#define debug_level_error 10
+#define debug_level_info 10
+
 #include <threads.h>
 
 #include <phantom_libc.h>
 
 #include "snap_sync.h"
-#include "hal.h"
+#include <hal.h>
 
-#define DEBUG 0
+//#define DEBUG 0
 
 /* This is set from snap code to ask us to hold our breath */
 
@@ -50,26 +67,27 @@ void phantom_thread_wait_4_snap( void )
 {
     if(phantom_virtual_machine_stop_request)
     {
-        if(DEBUG) printf("Thread will die now\n");
-        //pthread_exit(0);
+        SHOW_FLOW0( 4, "VM thread will die now");
         hal_exit_kernel_thread();
     }
 
-    if(DEBUG) printf("Thread will sleep for snap\n");
+    SHOW_FLOW0( 5, "VM thread will sleep for snap");
     hal_mutex_lock( &interlock_mutex );
 
     phantom_virtual_machine_threads_stopped++;
     hal_cond_broadcast( &phantom_snap_wait_4_vm_enter );
-    if(DEBUG) printf("Thread reported sleep, will wait now\n");
+
+    SHOW_FLOW0( 5, "VM thread reported sleep, will wait now");
+
     //while(phantom_virtual_machine_snap_request)
         hal_cond_wait( &phantom_vm_wait_4_snap, &interlock_mutex );
 
-    if(DEBUG) printf("Thread awaken, will report wakeup\n");
+    SHOW_FLOW0( 5, "VM thread awaken, will report wakeup");
     phantom_virtual_machine_threads_stopped--;
     hal_cond_broadcast( &phantom_snap_wait_4_vm_leave );
 
     hal_mutex_unlock( &interlock_mutex );
-    if(DEBUG) printf("Thread returns to activity\n");
+    SHOW_FLOW0( 5, "VM thread returns to activity");
 }
 
 /*
@@ -82,7 +100,7 @@ void phantom_thread_wait_4_snap( void )
 
 void phantom_snapper_wait_4_threads( void )
 {
-    if(DEBUG) printf("phantom_snapper_wait_4_threads\n");
+    SHOW_FLOW0( 5, "phantom_snapper_wait_4_threads");
 
     phantom_virtual_machine_snap_request++; // Ask them to go sleep
 
@@ -92,16 +110,16 @@ void phantom_snapper_wait_4_threads( void )
     {
 
         hal_mutex_lock( &interlock_mutex );
-        if(DEBUG) printf("Will wait for %d threads to stop, %d already did\n", threads_4_wait, phantom_virtual_machine_threads_stopped );
+        SHOW_FLOW( 5, "Will wait for %d threads to stop, %d already did", threads_4_wait, phantom_virtual_machine_threads_stopped );
         while( phantom_virtual_machine_threads_stopped < threads_4_wait )
         {
-            if(DEBUG) printf("Wait for thread\n");
+            SHOW_FLOW0( 5, "Wait for thread");
             hal_cond_wait( &phantom_snap_wait_4_vm_enter, &interlock_mutex );
             //hal_sleep_msec(2000);
-            if(DEBUG) printf("Woken up aft wait for %d threads, %d stopped\n", threads_4_wait, phantom_virtual_machine_threads_stopped);
+            SHOW_FLOW( 5, "Woken up aft wait for %d threads, %d stopped", threads_4_wait, phantom_virtual_machine_threads_stopped);
 
         }
-        if(DEBUG) printf("Finished waiting for threads\n");
+        SHOW_FLOW0( 5, "Finished waiting for threads");
         hal_mutex_unlock( &interlock_mutex );
 
 
@@ -111,14 +129,14 @@ void phantom_snapper_wait_4_threads( void )
         {
             // Yes - wait for new ones to stop as well
             threads_4_wait = newc;
-            if(DEBUG) printf("Found more threads (%d total), wait again\n", threads_4_wait);
+            SHOW_FLOW( 5, "Found more threads (%d total), wait again", threads_4_wait);
             continue;
         }
 
         break;
     }
 
-    if(DEBUG) printf("Snapper is free to snap\n");
+    SHOW_FLOW0( 5, "Snapper is free to snap");
 
 }
 
@@ -131,7 +149,7 @@ void phantom_snapper_wait_4_threads( void )
 
 void phantom_snapper_reenable_threads( void )
 {
-    if(DEBUG) printf("Snapper will reenable threads\n");
+    SHOW_FLOW0( 5, "Snapper will reenable threads");
     hal_mutex_lock( &interlock_mutex );
     phantom_virtual_machine_snap_request--; // May wake up now
 
@@ -142,22 +160,22 @@ void phantom_snapper_reenable_threads( void )
         return;
     }
 
-    if(DEBUG) printf("Snapper sleep request is %d, will broadcast\n", phantom_virtual_machine_snap_request);
+    SHOW_FLOW( 5, "Snapper sleep request is %d, will broadcast", phantom_virtual_machine_snap_request);
 
     hal_cond_broadcast( &phantom_vm_wait_4_snap );
 
 
-    if(DEBUG) printf("Snapper will wait for %d threads to awake\n", phantom_virtual_machine_threads_stopped);
+    SHOW_FLOW( 5, "Snapper will wait for %d threads to awake", phantom_virtual_machine_threads_stopped);
 
     while( phantom_virtual_machine_threads_stopped > 0 )
     {
         hal_cond_wait( &phantom_snap_wait_4_vm_leave, &interlock_mutex );
-        if(DEBUG) printf("Snapper: %d threads still sleep\n", phantom_virtual_machine_threads_stopped);
+        SHOW_FLOW( 5, "Snapper: %d threads still sleep", phantom_virtual_machine_threads_stopped);
 
     }
     hal_mutex_unlock( &interlock_mutex );
 
-    if(DEBUG) printf("Snapper done waiting for awake\n");
+    SHOW_FLOW0( 5, "Snapper done waiting for awake");
 
 }
 
@@ -184,17 +202,28 @@ void phantom_thread_sleep_worker( struct data_area_4_thread *thda )
 {
     if(phantom_virtual_machine_stop_request)
     {
-        if(DEBUG) printf("Thread will die now\n");
-        //pthread_exit(0);
+        SHOW_FLOW0( 5, "VM thread will die now");
         hal_exit_kernel_thread();
     }
 
-    if(DEBUG) printf("Thread will sleep for sleep\n");
+    SHOW_FLOW0( 5, "VM thread will sleep for sleep");
     hal_mutex_lock( &interlock_mutex );
 
     phantom_virtual_machine_threads_stopped++;
     hal_cond_broadcast( &phantom_snap_wait_4_vm_enter );
-    //if(DEBUG) printf("Thread reported sleep, will wait now\n");
+    //SHOW_FLOW0( 5, "VM thread reported sleep, will wait now");
+
+    if( thda->spin_to_unlock )
+    {
+        VM_SPIN_UNLOCK( (*thda->spin_to_unlock) );
+        thda->spin_to_unlock = 0;
+    }
+    else
+    {
+    if(thda->sleep_flag)
+        SHOW_ERROR(0, "Warn: vm th (da %x) sleep, no spin unlock", thda);
+    }
+
 
     //while(thda->sleep_flag)        hal_cond_wait( &(thda->wakeup_cond), &interlock_mutex );
     while(thda->sleep_flag)
@@ -202,21 +231,28 @@ void phantom_thread_sleep_worker( struct data_area_4_thread *thda )
 
 // TODO if snap is active someone still can wake us up - resleep for snap then!
 
-    //if(DEBUG) printf("Thread awaken, will report wakeup\n");
+    //SHOW_FLOW0( 5, "VM thread awaken, will report wakeup");
     phantom_virtual_machine_threads_stopped--;
 
     hal_mutex_unlock( &interlock_mutex );
-    if(DEBUG) printf("Thread awaken\n");
+    SHOW_FLOW0( 5, "VM thread awaken");
 }
 
 
-void phantom_thread_put_asleep( struct data_area_4_thread *thda )
+void phantom_thread_put_asleep( struct data_area_4_thread *thda, int *unlock_spin )
 {
+    hal_mutex_lock( &interlock_mutex );
+    // TODO atomic assign
+    if( thda->spin_to_unlock )
+        panic( "spin unlock > 1" );
+
+    thda->spin_to_unlock = unlock_spin;
     thda->sleep_flag++;
+    hal_mutex_unlock( &interlock_mutex );
     // NB! This will work if called from SYS only! That's
     // ok since no other bytecode instr can call this.
     // Real sleep happens in phantom_thread_sleep_worker
-printf("put thread asleep\n");
+    SHOW_FLOW0( 5, "put thread asleep");
 }
 
 
