97bb5a8320e53fb9dd90fddcac781c90ca8eb37a
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-09-27 10:32:51 +0000

    separate small funcs with alighn to aid better profiling

--- phantom/libkern/ia32/clisti.c
+++ phantom/libkern/ia32/clisti.c
@@ -7,15 +7,21 @@ void hal_wait_for_interrupt(void)
     __asm __volatile("hlt" : : );
 }
 
+__asm (".p2align 4;\n"); // for profiler to distingush funcs
+
 void        hal_cli()
 {
     __asm __volatile("cli" : : : "memory");
 }
 
+__asm (".p2align 4;\n"); // for profiler to distingush funcs
+
 void        hal_sti()
 {
     __asm __volatile("sti");
 }
+
+__asm (".p2align 4;\n"); // for profiler to distingush funcs
 
 int hal_is_sti()
 {
@@ -23,6 +29,8 @@ int hal_is_sti()
     __asm __volatile("pushfl; popl %0" : "=r" (ef));
     return ef & EFL_IF;
 }
+
+__asm (".p2align 4;\n"); // for profiler to distingush funcs
 
 int hal_save_cli()
 {
@@ -32,3 +40,5 @@ int hal_save_cli()
     __asm __volatile("cli" : : : "memory");
     return e;
 }
+
+__asm (".p2align 4;\n"); // for profiler to distingush funcs
