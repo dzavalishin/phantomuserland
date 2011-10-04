3c66a46f0faa58d7bd2cbdf96bb6124eabd693a7
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-10-04 12:50:29 +0000

    forgotten headers

---
+++ trunk/include/mips/arch/arch-flags.h
@@ -0,0 +1,25 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2011 Dmitry Zavalishin, dz@dz.ru
+ *
+ * Arch-specific hardware feature flags. Here we keep most
+ * frequently needed ones, such as SSE2 for ia32.
+ *
+ *
+**/
+
+#ifndef _ARCH_FLAGS_
+#define _ARCH_FLAGS_
+
+#ifndef ARCH_mips
+#warning Wrong arch?
+#endif
+
+
+//#define ARCH_IA32_APIC          (1<<0)
+//#define ARCH_IA32_SSE2          (1<<1)
+
+
+#endif // _ARCH_FLAGS_
---
+++ trunk/include/video/internal.h
@@ -0,0 +1,57 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2010 Dmitry Zavalishin, dz@dz.ru
+ *
+ * Internal video ops/definitions - needed mostly by windowing code.
+ *
+**/
+
+
+
+// -----------------------------------------------------------------------
+// windows lock - must be taken when manipulate all win q or on update
+// -----------------------------------------------------------------------
+
+
+void w_lock(void);
+void w_unlock(void);
+void w_assert_lock(void);
+
+
+// -----------------------------------------------------------------------
+// Screen repaint
+// -----------------------------------------------------------------------
+
+void 	drv_video_window_repaint_all(void);
+void    repaint_all_for_square( rect_t *todo );
+void    w_request_async_repaint( rect_t *r );
+
+
+// -----------------------------------------------------------------------
+// Default mouse implementation to be used by video drivers if no
+// hardware impl exists
+// -----------------------------------------------------------------------
+
+void drv_video_set_mouse_cursor_deflt(drv_video_bitmap_t *nc);
+void drv_video_draw_mouse_deflt(void);
+void drv_video_mouse_off_deflt(void);
+void drv_video_mouse_on_deflt(void);
+
+
+
+// -----------------------------------------------------------------------
+// vm stuff?
+// -----------------------------------------------------------------------
+
+
+struct data_area_4_bitmap;
+
+int drv_video_ppm_load( drv_video_bitmap_t **to, void *from );
+int drv_video_string2bmp( struct data_area_4_bitmap *bmp, void *_from );
+
+
+
+
+
