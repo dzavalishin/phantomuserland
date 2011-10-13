8a9998f82e39a499f32004248eed617c4cb8a88b
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-10-13 18:26:42 +0000

    resources

---
+++ plib/resources/buttons/defs.h
@@ -0,0 +1,8 @@
+extern drv_video_bitmap_t		close_bmp;
+extern drv_video_bitmap_t		close_pressed_bmp;
+extern drv_video_bitmap_t		pin_bmp;
+extern drv_video_bitmap_t		rollup_bmp;
+extern drv_video_bitmap_t		rollup_pressed_bmp;
+extern drv_video_bitmap_t		title_brown_bmp;
+extern drv_video_bitmap_t		title_green_bmp;
+extern drv_video_bitmap_t		title_violet_bmp;
