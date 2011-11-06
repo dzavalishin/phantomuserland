c2354a6c03a8f262b1468e1d518c747d869e84b5
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2011-11-06 11:24:37 +0000

    mips: started malta board definition

--- trunk/oldtree/kernel/phantom/mips/boards/mips-malta.c
+++ trunk/oldtree/kernel/phantom/mips/boards/mips-malta.c
@@ -4,7 +4,7 @@
  *
  * Copyright (C) 2005-2011 Dmitry Zavalishin, dz@dz.ru
  *
- * QEMU mipssim machine (-M mipssim) hardware mappings.
+ * QEMU mips Malta machine (-M malta) hardware mappings.
  *
  * Unfinished! Wrong! Move parts to arch dir?
  *
@@ -31,7 +31,7 @@
 #define debug_level_error 10
 #define debug_level_info 10
 
-char board_name[] = "mipssim";
+char board_name[] = "malta";
 
 void board_init_early(void)
 {
@@ -62,7 +62,7 @@ void board_interrupt_enable(int irq)
         arch_interrupt_enable(irq);
     else
     {
-#warning todo
+#warning todo 8259 cascaded
         SHOW_ERROR( 0, "unimpl irq %d", irq );
     }
 
@@ -157,7 +157,7 @@ static isa_probe_t board_drivers[] =
 //    { "COM1",		driver_isa_com_probe,   2, BOARD_ISA_IO|0x3F8u, 4 },
 
 #if HAVE_NET
-    { "MipsNet", 	driver_isa_mipsnet_probe,2, 0x4200, 2 },
+//    { "MipsNet", 	driver_isa_mipsnet_probe,2, 0x4200, 2 },
 
 #endif
 #endif
@@ -177,32 +177,10 @@ void board_make_driver_map(void)
 // -----------------------------------------------------------------------
 // stubs
 // -----------------------------------------------------------------------
-#if 0
-#warning stubs!
-void  paging_device_start_read_rq( void *pdev, void *pager_current_request, void *page_device_io_final_callback )
-{
-    (void) pdev;
-    (void) pager_current_request;
-    (void) page_device_io_final_callback;
-}
-
-void  paging_device_start_write_rq( void *pdev, void *pager_current_request, void *page_device_io_final_callback )
-{
-    (void) pdev;
-    (void) pager_current_request;
-    (void) page_device_io_final_callback;
-}
-
-void init_paging_device(void)
-{
-}
-#endif
 
 
 int phantom_dev_keyboard_get_key()
 {
-//#warning completely wrong!!!
-    //    return debug_console_getc();
     while(1)
         hal_sleep_msec(10000);
 }
@@ -210,7 +188,6 @@ int phantom_dev_keyboard_get_key()
 
 int driver_isa_vga_putc(int c )
 {
-    //debug_console_putc(c);
     return c;
 }
 
@@ -231,7 +208,7 @@ void board_fill_memory_map( amap_t *ram_map )
     int len = 256*1024*1024; // Hardcode 256M of RAM
     assert( 0 == amap_modify( ram_map, uptokernel, len-uptokernel, MEM_MAP_HI_RAM) );
 
-    assert( 0 == amap_modify( ram_map, kvtophys(BOARD_ISA_IO), BOARD_ISA_IO_LEN, MEM_MAP_DEV_MEM) );
+    //assert( 0 == amap_modify( ram_map, kvtophys(BOARD_ISA_IO), BOARD_ISA_IO_LEN, MEM_MAP_DEV_MEM) );
 }
 
 
