6c25d9ee81fa11760ff1aa7eaae53a84591efd99
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2012-05-02 19:38:10 +0000

    windows specifics

--- test/unit/suites/suite_skeleton.c
+++ test/unit/suites/suite_skeleton.c
@@ -1,17 +1,3 @@
-/* Первый тест-кейс */
-static void test_foo(void) {
-   /* Код тест-кейса */
-}
-
-/* Второй тест-кейс */
-static void test_foo2(void) {
-   /* Код тест-кейса */
-}
-
-void runSuite(void) {
-}
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <CUnit/Basic.h>
@@ -19,7 +5,7 @@ void runSuite(void) {
 #include "utils.h"
 
 TEST_FUNCT(foo) {
-    printf("test case 1\n");
+    printf("test case 1 - must fail\n");
      /* Фейковый код */
     CU_ASSERT_EQUAL(0, 1);
 }
