06854e3d1cb03347fa52249db08e8d1d52142294
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2012-06-26 08:47:08 +0000

    forgoten files

---
+++ trunk/oldtree/kernel/phantom/vm_cn_stats.c
@@ -0,0 +1,128 @@
+#define DEBUG_MSG_PREFIX "vmcn.stats"
+#include <debug_ext.h>
+#define debug_level_flow 10
+#define debug_level_error 10
+#define debug_level_info 10
+
+
+#include <vm/connect.h>
+#include <vm/alloc.h>
+#include <vm/p2c.h>
+#include <vm/syscall.h>
+
+#include <kernel/stats.h>
+
+#define CN_STS_OP_GET_CURR	0
+#define CN_STS_OP_GET_AVG       1
+#define CN_STS_OP_GET_THIS      2
+#define CN_STS_OP_GET_PREV      3
+#define CN_STS_OP_GET_ALLR      4
+
+#define CN_STS_OP_LAST          5
+
+
+
+
+errno_t cn_stats_do_operation( int op_no, struct data_area_4_connection *c, struct data_area_4_thread *t, pvm_object_t o )
+{
+    (void) t;
+    (void) c;
+    (void) o;
+
+    SHOW_ERROR( 1, "no op %d", op_no );
+    return EINVAL;
+}
+
+static pvm_object_t cn_stats_blocking_syscall_worker( pvm_object_t conn, struct data_area_4_thread *tc, int nmethod, pvm_object_t arg )
+{
+    (void) conn;
+    (void) tc;
+    (void) arg;
+
+    if( nmethod >= CN_STS_OP_LAST )
+    {
+        SHOW_ERROR( 1, "wrong op %d", nmethod );
+        return pvm_create_int_object(-1);
+    }
+
+    if(!IS_PHANTOM_INT(arg))
+    {
+        SHOW_ERROR0( 1, "arg not int" );
+        return pvm_create_int_object(-1);
+    }
+
+    int n_stat_counter = pvm_get_int(arg);
+    //ref_dec_o(o); wrapper does
+
+    if( n_stat_counter >= MAX_STAT_COUNTERS )
+    {
+        SHOW_ERROR( 1, "counter num %d > max", n_stat_counter );
+        return pvm_create_int_object(-1);
+    }
+
+    struct kernel_stats out;
+
+    errno_t e = get_stats_record( n_stat_counter, &out );
+
+    if( e )
+    {
+        SHOW_ERROR( 1, "err %d", e );
+        return pvm_create_int_object(-e);
+    }
+
+    int ret = 0;
+    switch(nmethod)
+    {
+    case CN_STS_OP_GET_CURR:
+        ret = out.current_per_second;
+        break;
+
+    case CN_STS_OP_GET_AVG:
+        ret = out.average_per_second;
+        break;
+
+    case CN_STS_OP_GET_THIS:
+        ret = out.total;
+        break;
+
+
+    case CN_STS_OP_GET_PREV:
+        ret = out.total_prev_runs;
+        break;
+
+    case CN_STS_OP_GET_ALLR:
+        ret = out.total_prev_and_this_runs;
+        break;
+    }
+
+    return pvm_create_int_object(ret);
+}
+
+
+// Init connection
+errno_t cn_stats_init( struct data_area_4_connection *c, struct data_area_4_thread *t )
+{
+    (void) t;
+
+    c->blocking_syscall_worker = cn_stats_blocking_syscall_worker;
+
+    //printf("Init stats");
+    return 0;
+}
+
+
+// Finish connection
+errno_t cn_stats_disconnect( struct data_area_4_connection *c, struct data_area_4_thread *t )
+{
+    (void) t;
+    (void) c;
+
+    //c->kernel = 0;
+
+    //SHOW_FLOW0( 1, "stats disconnected" );
+
+    return 0;
+}
+
+
+
---
+++ trunk/phantom/libwin/w_bg_win.c
@@ -0,0 +1,49 @@
+/**
+ *
+ * Phantom OS
+ *
+ * Copyright (C) 2005-2012 Dmitry Zavalishin, dz@dz.ru
+ *
+ * Windowing system - background window.
+ *
+**/
+#define DEBUG_MSG_PREFIX "win.bg"
+#include <debug_ext.h>
+#define debug_level_flow 0
+#define debug_level_error 10
+#define debug_level_info 10
+
+#include <video/screen.h>
+#include <video/window.h>
+#include <video/internal.h>
+#include <stdio.h>
+
+#if VIDEO_NEW_BG_WIN
+
+static window_handle_t back_win = 0;
+
+
+void w_bg_win_init(void)
+{
+    if(back_win)
+    {
+        SHOW_ERROR0( 0, "BG win inited twice" );
+        return;
+    }
+
+    //back_win = drv_video_window_create( scr_get_xsize(), scr_get_ysize(), 0, 0, COLOR_BLACK, "Background", WFLAG_WIN_DECORATED );
+    back_win = drv_video_window_create( scr_get_xsize(), scr_get_ysize(), 0, 0, COLOR_BLACK, "Background", WFLAG_WIN_NOFOCUS );
+
+    //back_win->flags &= ~WFLAG_WIN_DECORATED;
+    //back_win->flags |= WFLAG_WIN_NOFOCUS;
+
+    w_clear(back_win);
+    w_to_bottom(back_win);
+}
+
+window_handle_t w_get_bg_window(void)
+{
+    return back_win;
+}
+
+#endif
