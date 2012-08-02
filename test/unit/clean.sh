d95e3daa7c96c98919c876da9357995878e7c9b5
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2012-08-02 09:53:12 +0000

    cunit tests misc

--- test/unit/clean.sh
+++ test/unit/clean.sh
@@ -3,3 +3,4 @@ rm -rf CMakeFiles
 rm -rf suites/CMakeFiles
 rm -f cmake_install.cmake CMakeCache.txt Makefile
 rm -f suites/cmake_install.cmake suites/CMakeCache.txt suites/Makefile
+rm -f suites/*.dll suites/*.so runtests.exe runtests
