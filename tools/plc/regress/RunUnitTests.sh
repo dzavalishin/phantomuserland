4ae54c9a1fce95e89d0c96a8e7e6281e3d8adde8
dmitry.zavalishin <dmitry.zavalishin@f4b0950c-ff39-11dd-a7be-4b3ef3b0700c>
2013-03-03 17:50:31 +0000

    moving regress tests env from Soot

---
+++ tools/plc/regress/RunUnitTests.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+
+function do_real_java() {
+    fileName=$1;
+    className=${fileName/.java/};
+# -Xlint:-options disables bootstrap class path warning. or use --bootclasspath 
+    echo '     ---- Real Java run';
+    javac -source 1.4 -Xlint:-options $fileName;
+    java $className < input > $className.realOutput;
+}
+
+function java_to_phantom() {
+    fileName=$1;
+    className=${fileName/.java/};
+
+    echo '     ---- Make Phantom class files';
+    echo java -jar $PHANTOM_HOME/build/jar/plc.jar -o$PHANTOM_HOME/plib/bin -I$PHANTOM_HOME/plib/bin $className
+}
+
+function mytest() {
+    echo ' ---- ' $1;
+
+    do_real_java $1 ;
+    java_to_phantom $1 ;
+}
+
+function _mytest(){
+
+    echo 'starting function test';
+    i=$1;
+    j=${i/.java/};
+    if java -Xmx400M soot.Main -f J -src-prec java -app $j > $j.produceJimple;
+    then
+        echo $i 'Passed Producing Jimple';
+        if java -Xmx400M ru.dz.soot.SootMain -src-prec java -app $j > $j.produceClass;
+        then
+            echo $i 'Passed Producing Class';
+            javac -source 1.4 $i;
+            java $j < input > $j.realOutput;
+            if java -cp sootOutput $j < input > $j.myOutput;
+            then
+                echo $i 'Passed Running';
+                if diff $j.realOutput $j.myOutput > $j.diff;
+                then
+                    echo $i 'Passed Diff';
+                else
+                    echo $i 'Failed Diff';
+                fi
+                rm $j.diff;
+            else
+                echo $i 'Failed Running';
+            fi
+            rm $j.myOutput;
+            rm $j.realOutput;
+        else
+            echo $i 'Failed Producing Class';
+        fi
+        rm $j.produceClass;
+    else
+        echo $i 'Failed';
+    fi
+    rm $j.produceJimple;
+
+}
+
+echo 'starting regression tests';
+for i in *.java;
+do
+    mytest $i ;
+done
+
+
