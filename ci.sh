12c98f4734abbb855034b96f5df03dc5c5b2a1d5
Dmitry Zavalishin <dz@dz.ru>
2016-02-20 22:52:01 +0300

    ci

--- ci.sh
+++ ci.sh
@@ -3,7 +3,17 @@
 # you must stand in phantom home when you run this script
 #
 export PHANTOM_HOME="`pwd`"
+
+die ( ) {
+#	[ -s make.log ] && tail make.log
+	[ "$1" ] && echo "$*"
+	exit 0
+}
+
+
 #[ -L phantom/kernel ] || ln -s ../oldtree/kernel/phantom phantom/kernel
-make -C phantom "$@"
-make -C oldtree/kernel/phantom "$@"
-make -C test unit
+make -C phantom "$@" || die "Make libs failure"
+make -C oldtree/kernel/phantom "$@" || die "Make kernel failure"
+cd test
+make unit || die "Make unit failure"
+make plc || die "Make PLC failure"
