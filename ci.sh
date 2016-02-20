2e1c8661fc178d631e6ef9d6e559eb9c03be0307
Dmitry Zavalishin <dz@dz.ru>
2016-02-20 22:27:55 +0300

    continuos integration script

---
+++ ci.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# you must stand in phantom home when you run this script
+#
+export PHANTOM_HOME="`pwd`"
+#[ -L phantom/kernel ] || ln -s ../oldtree/kernel/phantom phantom/kernel
+make -C phantom "$@"
+make -C oldtree/kernel/phantom "$@"
+make -C test unit
