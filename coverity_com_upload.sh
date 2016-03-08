c0ea7ffa088de2cdce35444962db6107071bb1ef
Dmitry Zavalishin <dz@dz.ru>
2016-03-08 16:20:22 +0300

    coverity.com script

---
+++ coverity_com_upload.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+tar czvf phantom-cov.tgz cov-int
+curl --form token=$COVERITY_TOKEN \
+  --form email=dz@dz.ru \
+  --form file=phantom-cov.tgz \
+  --form version="Version" \
+  --form description="Phantom OS build" \
+  https://scan.coverity.com/builds?project=dzavalishin%2Fphantomuserland
