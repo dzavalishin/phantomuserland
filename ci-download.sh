0f193189b8efca328a40ce4f45f6f96665170df1
Dmitry Zavalishin <dz@dz.ru>
2016-03-08 16:22:34 +0300

    do not build qemu in CI

--- ci-download.sh
+++ ci-download.sh
@@ -2,11 +2,15 @@
 #
 # Not tested
 #
-wget http://wiki.qemu-project.org/download/qemu-1.0.1.tar.gz
-mkdir test/qemu
-cd test/qemu
-tar xf ../../qemu-1.0.1.tar.gz
-cd qemu-1.0.1
-sh ./configure
-make
-cd ../..
+#sudo yum install kvm libvirt python-virtinst qemu-kvm
+sudo yum install kvm qemu-kvm
+#
+#wget http://wiki.qemu-project.org/download/qemu-1.0.1.tar.gz
+#mkdir test/qemu
+#cd test/qemu
+#tar xf ../../qemu-1.0.1.tar.gz
+#cd qemu-1.0.1
+#sh ./configure
+#make
+#cd ../..
+
