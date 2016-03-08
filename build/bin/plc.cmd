ec0c1fb01b170a3ae20c0582ce01cbd12651e16d
Dmitry Zavalishin <dz@dz.ru>
2016-03-08 15:26:34 +0300

    utils cleanup

--- build/bin/plc.cmd
+++ build/bin/plc.cmd
@@ -1,2 +1,2 @@
-@java -jar %PHANTOM_HOME%/build/jar/plc.jar -o%PHANTOM_HOME%/plib/bin -I%PHANTOM_HOME%/plib/bin %1 %2 %3 %4 %5 %6 %7
-
+sh plc %*
+rem @java -jar %PHANTOM_HOME%/build/jar/plc.jar -o%PHANTOM_HOME%/plib/bin -I%PHANTOM_HOME%/plib/bin %1 %2 %3 %4 %5 %6 %7
