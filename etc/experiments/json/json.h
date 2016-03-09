1737bb61ae3bf3036c76b4055775732eae9a5dc0
dzavalishin <dz@dz.ru>
2016-03-09 18:41:25 +0300

    Sketch of JSON encoder

---
+++ etc/experiments/json/json.h
@@ -0,0 +1,30 @@
+
+typedef int (*json_out)(char c);
+
+struct json_output
+{
+    json_out    putc;
+    int         depth;
+};
+
+typedef struct json_output json_output;
+
+void json_start( json_output *jo );
+void json_stop( json_output *jo );
+
+
+void json_out_int( json_output *jo, const char *name, int value );
+void json_out_long( json_output *jo, const char *name, long value );
+void json_out_string( json_output *jo, const char *name, const char * value );
+
+
+void json_out_int_array( json_output *jo, const char *name, int *value );
+void json_out_string_array( json_output *jo, const char *name, const char * value );
+
+void json_out_open_struct( json_output *jo, const char *name );
+void json_out_close_struct( json_output *jo  );
+
+void json_out_open_array( json_output *jo, const char *name );
+void json_out_close_array( json_output *jo );
+
+
--- etc/experiments/json/mkjson.c
+++ etc/experiments/json/mkjson.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include "json.h"
 
 static inline char hex_nibble( char c ) { return (c >= 10) ? 'A' + c - 10 : '0' + c; }
 
-void out( char c )
+int _out( char c )
 {
     putchar( c );
 }
 
+json_output jo =
+{
+    _out,
+    0
+};
 
 // TODO UTF-8
 
-void jenc(char *in)
+void json_encode_string( json_output *jo, const char *in)
 {
+    jo->putc( '"' );
 
     while( *in )
     {
@@ -25,62 +32,152 @@ void jenc(char *in)
 
         case '\\':
         case '"':
-            out( '\\' );
-            out( c );
-            break;
-
         case '/':
-            out( '\\' );
-            out( c );
+            jo->putc( '\\' );
+            jo->putc( c );
             break;
 
         case '\b':
-            out( '\\' );
-            out( 'b' );
+            jo->putc( '\\' );
+            jo->putc( 'b' );
             break;
 
         case '\t':
-            out( '\\' );
-            out( 't' );
+            jo->putc( '\\' );
+            jo->putc( 't' );
             break;
 
         case '\n':
-            out( '\\' );
-            out( 'n' );
+            jo->putc( '\\' );
+            jo->putc( 'n' );
             break;
 
         case '\f':
-            out( '\\' );
-            out( 'f' );
+            jo->putc( '\\' );
+            jo->putc( 'f' );
             break;
 
         case '\r':
-            out( '\\' );
-            out( 'r' );
+            jo->putc( '\\' );
+            jo->putc( 'r' );
             break;
 
         default:
             if (c < ' ')
             {
-                out( '\\' );
-                out( 'u' );
+                jo->putc( '\\' );
+                jo->putc( 'u' );
                 hex_nibble( 0x0F & (c>>4) );
                 hex_nibble( 0x0F & c );
-
             }
             else
             {
-                out( c );
+                jo->putc( c );
             }
         }
     }
 
+    jo->putc( '"' );
+}
+
+void json_encode_int( json_output *jo, long v)
+{
+    // TODO loop me
+    if( v > 10 ) json_encode_int( jo, v/10 );
+    jo->putc( (v%10) + '0' );
+}
+
+
+void json_put_name( json_output *jo, const char *name )
+{
+    json_encode_string( jo, name );
+    jo->putc( ':' );
+    jo->putc( ' ' );
+}
+
+
+
+
+void json_out_int( json_output *jo, const char *name, int value )
+{
+    json_put_name( jo, name );
+    json_encode_int( jo, value );
+}
+
+void json_out_long( json_output *jo, const char *name, long value )
+{
+    json_put_name( jo, name );
+    json_encode_int( jo, value );
+}
+
+void json_out_string( json_output *jo, const char *name, const char * value )
+{
+    json_put_name( jo, name );
+    json_encode_string( jo, value );
+}
+
+
+void json_out_int_array( json_output *jo, const char *name, int *value );
+void json_out_string_array( json_output *jo, const char *name, const char * value );
+
+void json_out_open_struct( json_output *jo, const char *name )
+{
+    jo->depth++;
+    json_put_name( jo, name );
+    jo->putc( '{' );
+    jo->putc( '\n' );
+}
+
+void json_out_close_struct( json_output *jo  )
+{
+    jo->putc( '}' );
+    jo->putc( '\n' );
+    jo->depth--;
+}
+
+void json_out_open_array( json_output *jo, const char *name )
+{
+    jo->depth++;
+    json_put_name( jo, name );
+    jo->putc( '[' );
+    jo->putc( '\n' );
+}
+
+void json_out_close_array( json_output *jo )
+{
+    jo->putc( ']' );
+    jo->putc( '\n' );
+    jo->depth--;
+}
+
+
+void json_start( json_output *jo )
+{
+    jo->depth = 0;
+    jo->putc( '{' );
+    jo->putc( '\n' );
+}
+
+void json_stop( json_output *jo )
+{
+    if( jo->depth )
+        printf("JSON non-balanced\n");
+    jo->putc( '\n' );
+    jo->putc( '}' );
+    jo->putc( '\n' );
 }
 
 
 int main( int ac, char **av )
 {
-    jenc("hello \"world\"");
+    json_start( &jo );
+
+    //jenc("hello \"world\"");
+
+    json_out_string( &jo, "name", "DPC Runner" );
+    json_out_int( &jo, "tid", 1 );
+
+    json_stop( &jo );
 
     return 0;
 }
