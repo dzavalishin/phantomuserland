3bd4ebeb8290f67a05bf081f21219f3037083aac
Dmitry Zavalishin <dz@dz.ru>
2017-05-21 16:03:21 +0300

    fake cpu id

--- oldtree/kernel/phantom/e2k/sched.c
+++ oldtree/kernel/phantom/e2k/sched.c
@@ -14,3 +14,25 @@ phantom_scheduler_schedule_soft_irq()
 {
     hal_request_softirq(SOFT_IRQ_THREADS);
 }
+
+
+// TODO move to $(ARCH)/smp.c
+
+#include <kernel/smp.h>
+
+
+int GET_CPU_ID(void)
+{
+    return 0;
+/*
+        have_apic ?
+            ((apic_local_unit->unit_id.r >> 24) & 0xF)
+        :
+            0
+        ;
+*/
+}
+
+#if HAVE_SMP
+# error we're surely not ready for SMP
+#endif
