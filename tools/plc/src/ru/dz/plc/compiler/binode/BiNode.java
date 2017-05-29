c2aaa9f6f5720cb42db41c62041683142a8fe7c9
Dmitry Zavalishin <dz@dz.ru>
2017-05-29 20:34:16 +0300

    BiNode generate_C_code()

--- tools/plc/src/ru/dz/plc/compiler/binode/BiNode.java
+++ tools/plc/src/ru/dz/plc/compiler/binode/BiNode.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 import ru.dz.phantom.code.Codegen;
+import ru.dz.plc.compiler.C_codegen;
 import ru.dz.plc.compiler.CodeGeneratorState;
 import ru.dz.plc.compiler.LlvmCodegen;
 import ru.dz.plc.compiler.ParseState;
@@ -100,7 +101,22 @@ abstract public class BiNode extends Node {
 	    generateMyLlvmCode(llc);
 	}
 
-	
+	@Override
+	public void generate_C_code(C_codegen cgen) throws PlcException {
+		cTempName = cgen.getPhantomMethod().getLlvmTempName(this.getClass().getSimpleName());
+		
+	    if(context != null)			cgen.emitComment("Line "+context.getLineNumber());
+
+	    // Generate left child, then me, then right child
+	    // For example, l = const 5, r = read var, me = +
+	    // Result will be ((5) + (var))
+	    if( _l != null ) { _l.generate_C_code(cgen); }
+	    generateMy_C_Code(cgen);
+	    if( _r != null ) { _r.generate_C_code(cgen); }
+
+	    log.fine("Node "+this+" codegen");
+	    
+	}
 	
 }
 
