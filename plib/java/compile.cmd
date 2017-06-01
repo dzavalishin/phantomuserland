0d6c5cf52ca1be3949c6fa002f8fe31c5e569890
Dmitry Zavalishin <dz@dz.ru>
2017-06-02 02:41:24 +0300

    jpc run scripts

---
+++ plib/java/compile.cmd
@@ -0,0 +1,3 @@
+set f=src/test/toPhantom/testable.java
+call jc %f%
+call jpc -cbin;class test.toPhantom.Testable
---
+++ plib/java/jc.cmd
@@ -0,0 +1,2 @@
+@javac -g -cp class -cp bin -sourcepath src -bootclasspath class -d bin %1
+rem src/test/toPhantom/testable.java
