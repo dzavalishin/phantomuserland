12b39113af718911b7e6b6796f89eabf022d7b00
Dmitry Zavalishin <dz@dz.ru>
2017-06-07 11:40:27 +0300

    jpc compile all .class files in dir

--- tools/plc/src/ru/dz/soot/SootMain.java
+++ tools/plc/src/ru/dz/soot/SootMain.java
@@ -2,6 +2,11 @@ package ru.dz.soot;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -36,6 +41,7 @@ public class SootMain {
 	static Logger log = Logger.getLogger(SootMain.class.getName());
 	static ClassMap classes = ClassMap.get_map();
 	private static Set<String> classesToDo = new HashSet<String>();
+	private static String sourceClassPath;
 
 
 
@@ -122,14 +128,25 @@ public class SootMain {
 				{
 					if( a.charAt(1) == 'c' )
 					{
-						Scene.v().setSootClassPath(a.substring(2));
+						setSourceClassPath(a.substring(2));
+						continue;
+					}
+
+					if( a.charAt(1) == 'C' )
+					{
+						String dir = a.substring(2);
+						//say("-C "+dir);
+						//Scene.v().setSootClassPath(dir);
+						setSourceClassPath(dir);
+						queueAllClasses(dir);
 						continue;
 					}
 					
 					if( a.charAt(1) == '?' )
 					{
 						System.out.println(
-								"-c<java-class-path-list>\n"+
+								"-C<dir> - process all .class files in directory (sets -c dir also)\n"+
+								"-c<java-class-path-list> - set directory to load .class files from\n"+
 								"-o<phantom-class-out-dir>\n"+
 								"-I - ignored (plc compat)"
 								);
@@ -146,7 +163,7 @@ public class SootMain {
 		for( String iClass : classesToDo )
 		{
 			String cName = iClass.replace('/', '.');
-			say("Now process inner class "+cName);
+			say("Process queued class "+cName);
 			doClass(cName);
 		}
 
@@ -162,6 +179,90 @@ public class SootMain {
 		say("Generate Phantom code");
 		classes.preprocess();
 		classes.codegen();
+	}
+
+
+	private static void setSourceClassPath(String dir) {
+		sourceClassPath = dir;
+		Scene.v().setSootClassPath(dir);
+	}
+
+
+	private static void queueAllClasses(String dir) {
+		Path start = java.nio.file.Paths.get(dir);
+		FileVisitor<Path> visitor = new FileVisitor<Path>() {
+
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				//System.out.println("dir pre "+dir);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				//System.out.println("visit "+file);
+
+				if(!attrs.isRegularFile())
+				{
+					//System.out.println("not file "+file);
+					return FileVisitResult.CONTINUE;
+				}
+				
+				addPath(file);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+				System.err.println("scan failed "+file+" "+exc);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				//System.out.println("dir post "+dir);
+				return FileVisitResult.CONTINUE;
+			}
+
+		};
+		
+		try {
+			say("walk "+start);
+			Files.walkFileTree(start, visitor);
+		} catch (IOException e) {
+			System.err.println("Unable to scan "+start);
+			e.printStackTrace();
+		}
+		
+	}
+
+	// Add path to queue to be compiled
+	protected static void addPath(Path file) 
+	{	
+		Path start = java.nio.file.Paths.get(sourceClassPath);
+
+		//say("abs = "+file);
+		
+		//Path relative = file.relativize(start);
+		Path relative = start.relativize(file);
+		
+		//say("relative = "+relative);
+		
+		String className = relative.toString();
+		
+		className = className.replaceAll("/", ".");
+		className = className.replaceAll("\\\\", "."); // This means replace ONE backslash with .
+	
+		if( !className.endsWith(".class") )
+			return;
+		
+		// remove .class suffix
+		className = className.substring(0, className.length()-6);
+		
+		//say("add "+className);
+		
+		classesToDo.add(className);
+		
 	}
 
 
