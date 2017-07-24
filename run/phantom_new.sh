6095f8903044acaee874f7a3424407788508372c
SuperPrower <dya.eshshmai@gmail.com>
2017-07-24 13:44:24 +0300

    Added EFI option to QEMU config, moved grub cfg and fonts to outside folder just in case

--- run/phantom_new.sh
+++ run/phantom_new.sh
@@ -3,23 +3,25 @@
 # Test script for Phantom OS
 # Requires QEMU 2.5 or higher
 
-Q_BOOT="-boot order=c,menu=on "
-# Q_FLOPPY="-drive file=img/mygrubfloppy.img,format=raw,index=0,if=floppy "
-# Q_CD="-cdrom img/grub2.iso "
-#Q_DISK_A="-drive file=fat:rw:fat,format=raw,media=disk "
-Q_DISK_A="-drive file=phantom_boot.img,format=raw,media=disk "
+Q_DEBUG="-gdb tcp::1234  "
+Q_KVM="--enable-kvm "
+
+# Uncomment this to use EFI
+# Q_EFI="-L . --bios OVMF.fd "
+
+Q_BOOT="-boot order=d,menu=on "
+
+Q_CD="-cdrom img/grub2.img "
+Q_DISK_A="-drive file=fat:rw:fat,format=raw,media=disk "
+# Q_DISK_A="-drive file=phantom_boot.img,format=raw,media=disk "
 Q_DISK_B="-drive file=phantom.img,format=raw,media=disk "
+
 # Q_NET="-net nic,model=pcnet -net nic,model=rtl8139 -net user,tftp=tftp "
 # Q_PORTS=" -serial file:serial0.log"
 Q_PORTS="-serial stdio -soundhw pcspk "
-Q_DEBUG="-gdb tcp::1234 "
-Q_KVM="--enable-kvm"
+
 # VIO="-drive file=vio.img,if=virtio,format=raw -net nic,model=virtio"
 
-# rm serial0.log.old1
-# mv serial0.log.old serial0.log.old1
-# mv serial0.log serial0.log.old
-
-qemu-system-i386 $Q_KVM $Q_DEBUG -display sdl -m 256M $Q_PORTS $Q_BOOT $Q_FLOPPY $Q_CD $Q_DISK_A $Q_DISK_B $Q_NET $VIO
+qemu-system-i386 $Q_KVM $Q_DEBUG $Q_EFI -display sdl -m 256M $Q_PORTS $Q_BOOT $Q_CD $Q_DISK_A $Q_DISK_B $Q_NET $VIO
 
 exit
