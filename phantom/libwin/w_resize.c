cf7366f334dfc5ac3e7495bd287c2c30a40009c9
Dmitry Zavalishin <dz@dz.ru>
2019-10-19 01:32:46 +0300

    Overwrites end of VM object

--- phantom/libwin/w_resize.c
+++ phantom/libwin/w_resize.c
@@ -19,19 +19,60 @@
 #include <video/window.h>
 #include <video/internal.h>
 #include <video/zbuf.h>
+#include <video/vops.h>
 
 
 static void
-do_window_resize( drv_video_window_t *w, int xsize, int ysize )
+do_window_resize( drv_video_window_t *w, int xsize, int ysize, void *new_buffer, int clear )
 {
-    panic("not impl");
+
+    if(clear)
+    {
+        if( 0 == new_buffer )
+        {
+            if( (xsize * ysize) < (w->xsize * w->ysize))
+                new_buffer = w->bitmap;
+            else
+            {
+                free(w->bitmap); // TODO assert not persistent ptr
+                new_buffer = calloc( 1, drv_video_window_bytes(xsize,ysize) );
+            }            
+        }
+
+        w->bitmap = new_buffer;
+        iw_setup_buffers(w);
+        w_fill( w, w->bg );
+    }
+    else
+    {
+        int free_old = 0;
+        if( 0 == new_buffer )
+        {
+            new_buffer = calloc( 1, drv_video_window_bytes(xsize,ysize) );
+            free_old = 1;
+        }
+
+        w_fill_bitmap( new_buffer, w->bg, xsize*ysize );
+
+        // read from w_pixel?
+        bitmap2bitmap(
+            new_buffer, xsize, ysize, 0, 0,
+            w->r_pixel, w->xsize, w->ysize, 0, 0,
+            xsize, ysize
+        );
+
+        if(free_old) free(w->bitmap); // TODO assert not persistent ptr
+        w->bitmap = new_buffer;
+        iw_setup_buffers(w);
+    }
+    
 }
 
 
 
 
 void    
-w_resize( window_handle_t w, int xsize, int ysize, void *new_buffer, int clear )
+w_resize_ext( window_handle_t w, int xsize, int ysize, void *new_buffer, int clear )
 {
     rect_t oldsize;
     rect_t newsize;
@@ -40,10 +81,10 @@ w_resize( window_handle_t w, int xsize, int ysize, void *new_buffer, int clear )
     //w->state &= ~WSTATE_WIN_UNCOVERED; // mark as possibly covered
 
     w_get_bounds( w, &oldsize );
-    do_window_resize(w, xsize, ysize);
+    do_window_resize(w, xsize, ysize, new_buffer, clear);
     w_get_bounds( w, &newsize );
 
-    win_make_decorations( w ); // TODO clear dec?!
+    win_make_decorations( w ); // TODO clear old decorations?!
 
     rect_add( &maxsize, &oldsize, &newsize );
 
