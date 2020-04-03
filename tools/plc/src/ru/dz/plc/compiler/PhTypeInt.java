d3b91753d5a7f6e4fa3ff4aec2e3bb2ddbc0e83b
Dmitry Zavalishin <dz@dz.ru>
2020-04-03 12:38:38 +0300

    Update PhTypeInt.java

--- tools/plc/src/ru/dz/plc/compiler/PhTypeInt.java
+++ tools/plc/src/ru/dz/plc/compiler/PhTypeInt.java
@@ -3,7 +3,8 @@ package ru.dz.plc.compiler;
 public class PhTypeInt extends PhantomType {
 	public PhTypeInt() 
 	{
-		super( ClassMap.get_map().get(".internal.int",false,null) );	
+		super( ClassMap.get_map().get(".internal.int",false,null) );
+		//if( _class == null ) throw new PlcException("PhTypeInt", "class not loaded");
 		_is_int = true;
 	}
 
