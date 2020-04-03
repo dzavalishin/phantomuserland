eeb6e27e2476be3a6b4147e505bf3671ca84202d
Dmitry Zavalishin <dz@dz.ru>
2020-04-03 12:19:33 +0300

    Update Ast.java

--- tools/plc/src/ru/dz/bic/Ast.java
+++ tools/plc/src/ru/dz/bic/Ast.java
@@ -13,11 +13,6 @@ public class Ast {
 	private Lex l;
 	private static LinkedList<Token> tokens = new LinkedList<Token>();
 
-	/*
-	private final int id_ident;
-	private final int id_string_const;
-	private final int id_int_const;
-	*/
 	private BicBasicGrammar g;
 	
 	public Ast( String fname ) throws FileNotFoundException {
@@ -28,18 +23,6 @@ public class Ast {
 		l.set_input(fis);
 		
 		g = new BicBasicGrammar(l, fname);
-
-		/* / parametric
-		id_ident = Lex.get_id();
-		l.create_token(new TokenIdent(id_ident));
-
-		id_string_const = Lex.get_id();
-		l.create_token(new TokenStringConst(id_string_const));
-
-		id_int_const = Lex.get_id();
-		l.create_token(new TokenIntConst(id_int_const));
-		*/
-		
 		
 	}
 
